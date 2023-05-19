20564f712e0cdf364b63138a35c1c9780f9db1a3
JayanaGunaweera01 <jayana.20200003@iit.ac.lk>
2023-05-19 12:25:47 +0530

    Replaced paths

--- 1-migration-automation/1.1-ubuntu-os/change-deployment-toml-ubuntu.sh
+++ 1-migration-automation/1.1-ubuntu-os/change-deployment-toml-ubuntu.sh
@@ -1,27 +1,40 @@
 #!/bin/bash
 
-for file in $(find $DEPLOYMENT_PATH -type f -name 'deployment.toml');
-do
-cat $DEPLOYMENT_AUTOMATION > $file;
+# Define color variables
+GREEN='\033[0;32m\033[1m' # green color
+RESET='\033[0m' # reset color
 
+# Get the value of the inputs
+currentVersion=$3
+database=$5
+os=$6
+
+# Source env file
+chmod +x env.sh
+. ./env.sh
+echo "==> ${GREEN}Env file sourced successfully${RESET}"
+
+# Function to perform cat operation based on conditions
+perform_cat() {
+    cat_file=$1
+    deployment_file=$2
+
+    if [ "$database" = "mysql" ] && [ "$os" = "ubuntu" ] && [ "$currentVersion" = "$cat_file" ]; then
+        cat "$DEPLOYMENT_AUTOMATION_MYSQL_IS_${cat_file}" > "$deployment_file"
+    elif [ "$database" = "mssql" ] && [ "$os" = "ubuntu" ] && [ "$currentVersion" = "$cat_file" ]; then
+        cat "$DEPLOYMENT_AUTOMATION_MSSQL_IS_${cat_file}" > "$deployment_file"
+    elif [ "$database" = "postgres" ] && [ "$os" = "ubuntu" ] && [ "$currentVersion" = "$cat_file" ]; then
+        cat "$DEPLOYMENT_AUTOMATION_POSTGRE_IS_${cat_file}" > "$deployment_file"
+    fi
+}
+
+# Iterate over deployment files
+find "$DEPLOYMENT_PATH" -type f -name 'deployment.toml' | while read -r file; do
+    perform_cat "5.9" "$file"
+    perform_cat "5.10" "$file"
+    perform_cat "5.11" "$file"
+    perform_cat "6.0" "$file"
+    perform_cat "6.1" "$file"
+    perform_cat "6.2" "$file"
 done
 
-#Below block is for local setup
-
-#for file in $(find $DEPLOYMENT_PATH -type f -name 'deployment.toml');
-#do
-#awk '
-#BEGIN { skip = 0 }
-#{
-#if ($0 ~ /^#comment_start$/) {
-#skip = 1
-#}
-#if (!skip) {
-#print $0
-#}
-#if ($0 ~ /^#comment_end$/) {
-#skip = 0
-#}
-#}
-#' $DEPLOYMENT_AUTOMATION > $file
-#done
