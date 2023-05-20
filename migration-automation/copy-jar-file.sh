899264a69f0fd463b876bba56c3072b7e719351b
JayanaGunaweera01 <jayana.20200003@iit.ac.lk>
2023-05-21 01:11:51 +0530

    Did some minor changes

--- migration-automation/copy-jar-file.sh
+++ migration-automation/copy-jar-file.sh
@@ -4,65 +4,83 @@
 GREEN='\033[0;32m\033[1m' # green color
 RESET='\033[0m'           # reset color
 
-# Method to source the relevant env file based on the OS input
-source_env_file() {
-  local os="$1"
-  
-  case "$os" in
-    "ubuntu-latest")
-      cd "/home/runner/work/Automating-Product-Migration-Testing/Automating-Product-Migration-Testing/migration-automation"
-      chmod +x env.sh
-      . ./env.sh
-      echo -e "${GREEN}==> Env file sourced successfully${RESET}"
-      ;;
-    "macos-latest")
-      cd "/Users/runner/work/Automate-Product-Migration-Testing/Automate-Product-Migration-Testing/01-Migration-Automation"
-      source ./env.sh
-      echo -e "${GREEN}==> Env file sourced successfully${RESET}"
-      ;;
-    "windows-latest")
-      cd "D:\a\Automating-Product-Migration-Testing\Automating-Product-Migration-Testing\migration-automation"
-      . ./env.sh
-      echo -e "${GREEN}==> Env file sourced successfully${RESET}"
-      ;;
-    *)
-      echo -e "${GREEN}==> Unknown OS: $os${RESET}"
-      ;;
-  esac
-}
+# Get the values of the inputs
+database=$5
+os=$6
 
-# Method to copy the relevant JDBC driver to the target directory based on the database input
+# Method to copy the relevant JDBC driver to the target directory based on the database and OS input
 copy_jdbc_driver() {
-  local database="$1"
-
-  case "$database" in
-    "mysql")
+  case "$database-$os" in
+    "mysql-ubuntu-latest")
       cp -r "$JAR_MYSQL" "$LIB"
       ;;
-    "mssql")
+    "mssql-ubuntu-latest")
       cp -r "$JAR_MSSQL" "$LIB"
       ;;
-    "postgres")
+    "postgres-ubuntu-latest")
       cp -r "$JAR_POSTGRE" "$LIB"
+      ;;
+    "mysql-macos-latest")
+      cp -r "$JAR_MYSQL_MAC" "$LIB_MAC"
+      ;;
+    "mssql-macos-latest")
+      cp -r "$JAR_MSSQL_MAC" "$LIB_MAC"
+      ;;
+    "postgres-macos-latest")
+      cp -r "$JAR_POSTGRE_MAC" "$LIB_MAC"
+      ;;
+    "mysql-windows-latest")
+      Copy-Item -Path "D:\a\Automate-Product-Migration-Testing\Automate-Product-Migration-Testing\utils\mysql-connector-java-8.0.29.jar" -Destination "D:\a\Automate-Product-Migration-Testing\Automate-Product-Migration-Testing\01-Migration-Automation\IS_HOME_OLD\wso2is-5.11.0\repository\components\lib\"
+      Write-Host "`e[0;32m`e[1mPlaced JDBC driver successfully`e[0m"
+      ;;
+    *)
+      echo "${GREEN}==> Unknown combination of database and OS: $database-$os${RESET}"
       ;;
   esac
 
+  # Determine the JDBC driver file path based on the database and OS
+  driver_file=""
+  if [ "$os" == "ubuntu-latest" ]; then
+    case "$database" in
+      "mysql")
+        driver_file="$LIB/$JAR_MYSQL"
+        ;;
+      "mssql")
+        driver_file="$LIB/$JAR_MSSQL"
+        ;;
+      "postgres")
+        driver_file="$LIB/$JAR_POSTGRE"
+        ;;
+    esac
+  elif [ "$os" == "macos-latest" ]; then
+    case "$database" in
+      "mysql")
+        driver_file="$LIB_MAC/$JAR_MYSQL_MAC"
+        ;;
+      "mssql")
+        driver_file="$LIB_MAC/$JAR_MSSQL_MAC"
+        ;;
+      "postgres")
+        driver_file="$LIB_MAC/$JAR_POSTGRE_MAC"
+        ;;
+    esac
+  elif [ "$os" == "windows-latest" ]; then
+    case "$database" in
+      "mysql")
+        driver_file="D:\a\Automate-Product-Migration-Testing\Automate-Product-Migration-Testing\01-Migration-Automation\IS_HOME_OLD\wso2is-5.11.0\repository\components\lib\mysql-connector-java-8.0.29.jar"
+        ;;
+    esac
+  fi
+
   # Wait for the JDBC driver to be copied to the lib folder
-  while [ ! -f "$LIB/$database-jdbc-driver.jar" ]; do
-    echo -e "${GREEN}==> JDBC driver not found in lib folder, waiting...${RESET}"
+  while [ ! -f "$driver_file" ]
+  do
+    echo "${GREEN}==> JDBC driver not found in lib folder, waiting...${RESET}"
     sleep 5
   done
 
-  echo -e "${GREEN}==> JDBC driver found in lib folder, continuing...${RESET}"
+  echo "${GREEN}==> JDBC driver found in lib folder, continuing...${RESET}"
 }
 
-
-# Get the values of the inputs
-database="$5"
-os="$6"
-
-# Source the relevant env file based on the OS input
-source_env_file "$os"
-
-# Copy the relevant JDBC driver to the target directory based on the database input
-copy_jdbc_driver "$database"
+# Copy the relevant JDBC driver to the target directory based on the database and OS input
+copy_jdbc_driver
