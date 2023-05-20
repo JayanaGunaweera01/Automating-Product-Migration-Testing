35f29d4dce42b76ecac114c1032548a92cf5407d
JayanaGunaweera01 <jayana.20200003@iit.ac.lk>
2023-05-21 01:45:33 +0530

    Did some minor changes

--- migration-automation/ubuntu-os/migration-script-ubuntu.sh
+++ migration-automation/ubuntu-os/migration-script-ubuntu.sh
@@ -120,7 +120,7 @@ echo "${GREEN}==> Deployment.toml changed successfully${RESET}"
 # Setup mysql
 cd "$AUTOMATION_HOME"
 chmod +x setup-mysql.sh
-sh setup-mysql.sh 
+sh setup-mysql.sh &
 wait $
 
 # Copy Jars 
