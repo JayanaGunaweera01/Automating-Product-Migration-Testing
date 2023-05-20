da2a201cc827d89a7a2a386f47b8e0b87d000ec3
JayanaGunaweera01 <jayana.20200003@iit.ac.lk>
2023-05-21 02:05:08 +0530

    Did some minor changes

--- migration-automation/copy-jar-file.sh
+++ migration-automation/copy-jar-file.sh
@@ -29,13 +29,6 @@ copy_jdbc_driver() {
     "postgres-macos-latest")
       cp -r "$JAR_POSTGRE_MAC" "$LIB_MAC"
       ;;
-    "mysql-windows-latest")
-      Copy-Item -Path "D:\a\Automate-Product-Migration-Testing\Automate-Product-Migration-Testing\utils\mysql-connector-java-8.0.29.jar" -Destination "D:\a\Automate-Product-Migration-Testing\Automate-Product-Migration-Testing\01-Migration-Automation\IS_HOME_OLD\wso2is-5.11.0\repository\components\lib\"
-      Write-Host "`e[0;32m`e[1mPlaced JDBC driver successfully`e[0m"
-      ;;
-    *)
-      echo "${GREEN}==> Unknown combination of database and OS: $database-$os${RESET}"
-      ;;
   esac
 
   # Determine the JDBC driver file path based on the database and OS
@@ -64,17 +57,10 @@ copy_jdbc_driver() {
         driver_file="$JAR_POSTGRE_MAC"
         ;;
     esac
-  elif [ "$os" == "windows-latest" ]; then
-    case "$database" in
-      "mysql")
-        driver_file="D:\a\Automate-Product-Migration-Testing\Automate-Product-Migration-Testing\01-Migration-Automation\IS_HOME_OLD\wso2is-5.11.0\repository\components\lib\mysql-connector-java-8.0.29.jar"
-        ;;
-    esac
   fi
 
   # Wait for the JDBC driver to be copied to the lib folder
-  while [ ! -f "$driver_file" ]
-  do
+  while [ ! -f "$driver_file" ]; do
     echo "${GREEN}==> JDBC driver not found in lib folder, waiting...${RESET}"
     sleep 5
   done
