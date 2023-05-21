1d1f053bdd4f729e30add83552100b47f33612ca
JayanaGunaweera01 <jayana.20200003@iit.ac.lk>
2023-05-21 15:47:12 +0530

    Did some minor changes

--- migration-automation/ubuntu-os/change-deployment-toml-ubuntu.sh
+++ migration-automation/ubuntu-os/change-deployment-toml-ubuntu.sh
@@ -17,23 +17,30 @@ echo "==> ${GREEN}Env file sourced successfully${RESET}"
 # Function to perform cat operation based on conditions
 perform_cat() {
     cat_file=$1
+    cat_variable=$2
+    deployment_file=$3
 
     if [ "$database" = "mysql" ] && [ "$os" = "ubuntu-latest" ] && [ "$currentVersion" = "$cat_file" ]; then
-        cat "$DEPLOYMENT_AUTOMATION_MYSQL_IS_${cat_file}" > "$DEPLOYMENT_PATH"
-        echo "Content in 'DEPLOYMENT_AUTOMATION_MYSQL_IS_${cat_file}' catenated to 'DEPLOYMENT_AUTOMATION'."
+        cat_variable_value=$(eval echo "\$DEPLOYMENT_AUTOMATION_MYSQL_IS_${cat_variable}")
+        echo "$cat_variable_value" > "$deployment_file"
+        echo "Deployment file '$deployment_file' replaced."
     elif [ "$database" = "mssql" ] && [ "$os" = "ubuntu-latest" ] && [ "$currentVersion" = "$cat_file" ]; then
-        cat "$DEPLOYMENT_AUTOMATION_MSSQL_IS_${cat_file}" > "$DEPLOYMENT_PATH"
-        echo "Content in 'DEPLOYMENT_AUTOMATION_MSSQL_IS_${cat_file}' catenated to 'DEPLOYMENT_AUTOMATION'."
+        cat_variable_value=$(eval echo "\$DEPLOYMENT_AUTOMATION_MSSQL_IS_${cat_variable}")
+        echo "$cat_variable_value" > "$deployment_file"
+        echo "Deployment file '$deployment_file' replaced."
     elif [ "$database" = "postgres" ] && [ "$os" = "ubuntu-latest" ] && [ "$currentVersion" = "$cat_file" ]; then
-        cat "$DEPLOYMENT_AUTOMATION_POSTGRE_IS_${cat_file}" > "$DEPLOYMENT_PATH"
-        echo "Content in 'DEPLOYMENT_AUTOMATION_POSTGRE_IS_${cat_file}' catenated to 'DEPLOYMENT_AUTOMATION'."
+        cat_variable_value=$(eval echo "\$DEPLOYMENT_AUTOMATION_POSTGRE_IS_${cat_variable}")
+        echo "$cat_variable_value" > "$deployment_file"
+        echo "Deployment file '$deployment_file' replaced."
     fi
 }
 
-# Perform cat operation based on conditions
-perform_cat "5.9.0"
-perform_cat "5.10.0"
-perform_cat "5.11.0"
-perform_cat "6.0.0"
-perform_cat "6.1.0"
-perform_cat "6.2.0"
+# Iterate over deployment files
+find "$DEPLOYMENT_PATH" -type f -name 'deployment.toml' | while IFS= read -r file; do
+    perform_cat "5.9.0" "5_9" "$file"
+    perform_cat "5.10.0" "5_10" "$file"
+    perform_cat "5.11.0" "5_11" "$file"
+    perform_cat "6.0.0" "6_0" "$file"
+    perform_cat "6.1.0" "6_1" "$file"
+    perform_cat "6.2.0" "6_2" "$file"
+done
