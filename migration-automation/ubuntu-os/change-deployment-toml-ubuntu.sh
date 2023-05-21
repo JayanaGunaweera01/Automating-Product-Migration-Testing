6c82e1f0b078e8f0483b72d56898fa8603731a75
JayanaGunaweera01 <jayana.20200003@iit.ac.lk>
2023-05-22 01:48:00 +0530

    Did some minor changes

--- migration-automation/ubuntu-os/change-deployment-toml-ubuntu.sh
+++ migration-automation/ubuntu-os/change-deployment-toml-ubuntu.sh
@@ -15,33 +15,33 @@ chmod +x env.sh
 . ./env.sh
 echo -e "${GREEN}==> Env file sourced successfully${RESET}"
 
-if [ "$database" = "mysql" ] && [ "$os" = "ubuntu-latest" ]; then
-    if [ "$currentVersion" = "5.9.0" ]; then
+if [ database = "mysql" ] && [ os = "ubuntu-latest" ]; then
+    if [ currentVersion = "5.9.0" ]; then
         for file in $(find "$DEPLOYMENT_PATH" -type f -name 'deployment.toml'); do
             cat "$DEPLOYMENT_AUTOMATION_MYSQL_UBUNTU_IS_5_9" >"$file"
         done
         echo -e "Deployment file for $currentVersion replaced successfully."
-    elif [ "$currentVersion" = "5.10.0" ]; then
+    elif [ currentVersion = "5.10.0" ]; then
         for file in $(find "$DEPLOYMENT_PATH" -type f -name 'deployment.toml'); do
             cat "$DEPLOYMENT_AUTOMATION_MYSQL_UBUNTU_IS_5_10" >"$file"
         done
         echo -e "Deployment file for $currentVersion replaced successfully."
-    elif [ "$currentVersion" = "5.11.0" ]; then
+    elif [ currentVersion = "5.11.0" ]; then
         for file in $(find "$DEPLOYMENT_PATH" -type f -name 'deployment.toml'); do
             cat "$DEPLOYMENT_AUTOMATION_MYSQL_UBUNTU_IS_5_11" >"$file"
         done
         echo -e "Deployment file for $currentVersion replaced successfully."
-    elif [ "$currentVersion" = "6.0.0" ]; then
+    elif [ currentVersion = "6.0.0" ]; then
         for file in $(find "$DEPLOYMENT_PATH" -type f -name 'deployment.toml'); do
             cat "$DEPLOYMENT_AUTOMATION_MYSQL_UBUNTU_IS_6_0" >"$file"
         done
         echo -e "Deployment file for $currentVersion replaced successfully."
-    elif [ "$currentVersion" = "6.1.0" ]; then
+    elif [ currentVersion = "6.1.0" ]; then
         for file in $(find "$DEPLOYMENT_PATH" -type f -name 'deployment.toml'); do
             cat "$DEPLOYMENT_AUTOMATION_MYSQL_UBUNTU_IS_6_1" >"$file"
         done
         echo -e "Deployment file for $currentVersion replaced successfully."
-    elif [ "$currentVersion" = "6.2.0" ]; then
+    elif [ currentVersion = "6.2.0" ]; then
         for file in $(find "$DEPLOYMENT_PATH" -type f -name 'deployment.toml'); do
             cat "$DEPLOYMENT_AUTOMATION_MYSQL_UBUNTU_IS_6_2" >"$file"
         done
