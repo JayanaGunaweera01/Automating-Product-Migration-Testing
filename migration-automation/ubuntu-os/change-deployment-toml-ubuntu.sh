f74ad6ec6d48a5a28d3ae5e11dab7635ffc2b33b
JayanaGunaweera01 <jayana.20200003@iit.ac.lk>
2023-05-21 21:30:14 +0530

    Did some minor changes

--- migration-automation/ubuntu-os/change-deployment-toml-ubuntu.sh
+++ migration-automation/ubuntu-os/change-deployment-toml-ubuntu.sh
@@ -10,37 +10,34 @@ database=$5
 os=$6
 
 # Source env file
+cd "/home/runner/work/Automating-Product-Migration-Testing/Automating-Product-Migration-Testing/migration-automation"
 chmod +x env.sh
 . ./env.sh
-echo "==> ${GREEN}Env file sourced successfully${RESET}"
+echo "${GREEN}==> Env file sourced successfully${RESET}"
 
 # Function to perform cat operation based on conditions
 perform_cat() {
     cat_file=$1
-    cat_variable=$2
-    deployment_file=$3
+    deployment_file=$2
 
-    if [ "$database" = "mysql" ] && [ "$os" = "ubuntu-latest" ] && [ "$currentVersion" = "$cat_file" ]; then
-        cat_variable_value=$(eval echo "\$DEPLOYMENT_AUTOMATION_MYSQL_IS_${cat_variable}")
-        echo "$cat_variable_value" > "$deployment_file"
+    if [ "$database" = "mysql" ] && [ "$os" = "ubuntu" ] && [ "$currentVersion" = "$cat_file" ]; then
+        cat "$DEPLOYMENT_AUTOMATION_MYSQL_UBUNTU_IS_${cat_file}" >"$deployment_file"
         echo "Deployment file '$deployment_file' replaced."
-    elif [ "$database" = "mssql" ] && [ "$os" = "ubuntu-latest" ] && [ "$currentVersion" = "$cat_file" ]; then
-        cat_variable_value=$(eval echo "\$DEPLOYMENT_AUTOMATION_MSSQL_IS_${cat_variable}")
-        echo "$cat_variable_value" > "$deployment_file"
+    elif [ "$database" = "mssql" ] && [ "$os" = "ubuntu" ] && [ "$currentVersion" = "$cat_file" ]; then
+        cat "$DEPLOYMENT_AUTOMATION_MSSQL_UBUNTU_IS_${cat_file}" >"$deployment_file"
         echo "Deployment file '$deployment_file' replaced."
-    elif [ "$database" = "postgres" ] && [ "$os" = "ubuntu-latest" ] && [ "$currentVersion" = "$cat_file" ]; then
-        cat_variable_value=$(eval echo "\$DEPLOYMENT_AUTOMATION_POSTGRE_IS_${cat_variable}")
-        echo "$cat_variable_value" > "$deployment_file"
+    elif [ "$database" = "postgres" ] && [ "$os" = "ubuntu" ] && [ "$currentVersion" = "$cat_file" ]; then
+        cat "$DEPLOYMENT_AUTOMATION_POSTGRE_UBUNTU_IS_${cat_file}" >"$deployment_file"
         echo "Deployment file '$deployment_file' replaced."
     fi
 }
 
 # Iterate over deployment files
-find "$DEPLOYMENT_PATH" -type f -name 'deployment.toml' | while IFS= read -r file; do
-    perform_cat "5.9.0" "5_9" "$file"
-    perform_cat "5.10.0" "5_10" "$file"
-    perform_cat "5.11.0" "5_11" "$file"
-    perform_cat "6.0.0" "6_0" "$file"
-    perform_cat "6.1.0" "6_1" "$file"
-    perform_cat "6.2.0" "6_2" "$file"
+find "$DEPLOYMENT_PATH" -type f -name 'deployment.toml' | while read -r file; do
+    perform_cat "5_9" "$file"
+    perform_cat "5_10" "$file"
+    perform_cat "5_11" "$file"
+    perform_cat "6_0" "$file"
+    perform_cat "6_1" "$file"
+    perform_cat "6_2" "$file"
 done
