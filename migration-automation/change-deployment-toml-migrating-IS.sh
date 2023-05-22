a77f005c68dc53692d294585157334ac53eac7a9
JayanaGunaweera01 <jayana.20200003@iit.ac.lk>
2023-05-22 20:56:13 +0530

    Did some minor changes

--- migration-automation/change-deployment-toml-migrating-IS.sh
+++ migration-automation/change-deployment-toml-migrating-IS.sh
@@ -5,21 +5,21 @@ GREEN='\033[0;32m\033[1m' # green color
 RESET='\033[0m'           # reset color
 
 # Get the value of the inputs
-currentVersion=$1
+migratingVersion=$1
 database=$2
 os=$3
 
 # Set deployment file and path based on OS
 if [ "$os" = "ubuntu-latest" ]; then
-    deployment_file="$DEPLOYMENT_PATH/deployment.toml"
-    deployment_path="$DEPLOYMENT_PATH"
+    deployment_file="$DEPLOYMENT_PATH_NEW/deployment.toml"
+    deployment_path="$DEPLOYMENT_PATH_NEW"
     cd "/home/runner/work/Automating-Product-Migration-Testing/Automating-Product-Migration-Testing/migration-automation" || exit 1
    chmod +x env.sh
    . ./env.sh
     echo "${GREEN}==> Env file for Ubuntu sourced successfully${RESET}"
 elif [ "$os" = "macos-latest" ]; then
-    deployment_file="$DEPLOYMENT_PATH_MAC/deployment.toml"
-    deployment_path="$DEPLOYMENT_PATH_MAC"
+    deployment_file="$DEPLOYMENT_PATH_NEW_MAC/deployment.toml"
+    deployment_path="$DEPLOYMENT_PATH_NEW_MAC"
     cd "/Users/runner/work/Automating-Product-Migration-Testing/Automating-Product-Migration-Testing/migration-automation" || exit 1
     chmod +x env.sh
     source ./env.sh
@@ -29,7 +29,7 @@ fi
 # Set deployment automation file based on database and OS
 if [ "$database" = "mysql" ]; then
     if [ "$os" = "ubuntu-latest" ]; then
-        case "$currentVersion" in
+        case "$migratingVersion" in
             "5.9.0")
                 deployment_automation_file="$DEPLOYMENT_AUTOMATION_MYSQL_UBUNTU_IS_5_9"
                 ;;
@@ -50,7 +50,7 @@ if [ "$database" = "mysql" ]; then
                 ;;
         esac
     elif [ "$os" = "macos-latest" ]; then
-        case "$currentVersion" in
+        case "$migratingVersion" in
             "5.9.0")
                 deployment_automation_file="$DEPLOYMENT_AUTOMATION_MYSQL_MAC_IS_5_9"
                 ;;
@@ -73,7 +73,7 @@ if [ "$database" = "mysql" ]; then
     fi
 elif [ "$database" = "postgres" ]; then
     if [ "$os" = "ubuntu-latest" ]; then
-        case "$currentVersion" in
+        case "$migratingVersion" in
             "5.9.0")
                 deployment_automation_file="$DEPLOYMENT_AUTOMATION_POSTGRES_UBUNTU_IS_5_9"
                 ;;
@@ -94,7 +94,7 @@ elif [ "$database" = "postgres" ]; then
                 ;;
         esac
     elif [ "$os" = "macos-latest" ]; then
-        case "$currentVersion" in
+        case "$migratingVersion" in
             "5.9.0")
                 deployment_automation_file="$DEPLOYMENT_AUTOMATION_POSTGRES_MAC_IS_5_9"
                 ;;
@@ -117,7 +117,7 @@ elif [ "$database" = "postgres" ]; then
     fi
 elif [ "$database" = "mssql" ]; then
     if [ "$os" = "ubuntu-latest" ]; then
-        case "$currentVersion" in
+        case "$migratingVersion" in
             "5.9.0")
                 deployment_automation_file="$DEPLOYMENT_AUTOMATION_MSSQL_UBUNTU_IS_5_9"
                 ;;
@@ -138,7 +138,7 @@ elif [ "$database" = "mssql" ]; then
                 ;;
         esac
     elif [ "$os" = "macos-latest" ]; then
-        case "$currentVersion" in
+        case "$migratingVersion" in
             "5.9.0")
                 deployment_automation_file="$DEPLOYMENT_AUTOMATION_MSSQL_MAC_IS_5_9"
                 ;;
--- migration-automation/ubuntu-os/migration-script-ubuntu.sh
+++ migration-automation/ubuntu-os/migration-script-ubuntu.sh
@@ -247,9 +247,9 @@ cp -r "$USERSTORE_OLD_PATH" "$USERSTORE_NEW_PATH"
 echo "${BLUE}==> Userstores from IS $3 to IS $4 copied successfully!${RESET}"
 
 # Deployment toml changes in new is version
-cd "$UBUNTU_PATH"
-chmod +x change-deployment-toml-ubuntu-new.sh
-sh change-deployment-toml-ubuntu-new.sh
+chmod +x change-deployment-toml-migrating-IS.sh
+sh change-deployment-toml-migrating-IS.sh "$4" "$5" "$6"
+echo "${GREEN}==> Deployment.toml changed successfully${RESET}"
 echo "${BLUE}==> Copied deployment toml of $3 to $4 successfully!${RESET}"
 
 # Check if all files are copied successfully
