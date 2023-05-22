a328cb1b10eb0e2f5aad3da005941d7c3d54939f
JayanaGunaweera01 <jayana.20200003@iit.ac.lk>
2023-05-22 14:47:13 +0530

    Did some minor changes

--- migration-automation/ubuntu-os/change-deployment-toml-ubuntu.sh
+++ migration-automation/ubuntu-os/change-deployment-toml-ubuntu.sh
@@ -9,66 +9,72 @@ currentVersion=$1
 database=$2
 os=$3
 
-version1="5.9.0"
-version2="5.10.0"
-version3="5.11.0"
-version4="6.0.0"
-version5="6.1.0"
-version6="6.2.0"
+#!/bin/bash
 
+# Set deployment file and path based on OS
 if [ "$os" = "ubuntu-latest" ]; then
     deployment_file="$DEPLOYMENT_PATH/deployment.toml"
-
-    # Source env file Ubuntu
-    cd "/home/runner/work/Automating-Product-Migration-Testing/Automating-Product-Migration-Testing/migration-automation"
+    deployment_path="$DEPLOYMENT_PATH"
+    cd "/home/runner/work/Automating-Product-Migration-Testing/Automating-Product-Migration-Testing/migration-automation" || exit 1
     chmod +x env.sh
-    . ./env.sh
+    source ./env.sh
     echo "${GREEN}==> Env file for Ubuntu sourced successfully${RESET}"
-
 elif [ "$os" = "macos-latest" ]; then
     deployment_file="$DEPLOYMENT_PATH_MAC/deployment.toml"
-
-    # Source env file Mac
-    cd "/Users/runner/work/Automating-Product-Migration-Testing/Automating-Product-Migration-Testing/migration-automation"
+    deployment_path="$DEPLOYMENT_PATH_MAC"
+    cd "/Users/runner/work/Automating-Product-Migration-Testing/Automating-Product-Migration-Testing/migration-automation" || exit 1
     source ./env.sh
     echo "${GREEN}==> Env file for Mac sourced successfully${RESET}"
 fi
 
+# Set deployment automation file based on database and OS
+case "$currentVersion" in
+    "5.9.0")
+        if [ "$database" = "mysql" ] && [ "$os" = "ubuntu-latest" ]; then
+            deployment_automation_file="${DEPLOYMENT_AUTOMATION_PATH}/MYSQL_UBUNTU_IS_5_9"
+        elif [ "$database" = "mysql" ] && [ "$os" = "macos-latest" ]; then
+            deployment_automation_file="${DEPLOYMENT_AUTOMATION_PATH}/MYSQL_MAC_IS_5_9"
+        fi
+        ;;
+    "5.10.0")
+        if [ "$database" = "mysql" ] && [ "$os" = "ubuntu-latest" ]; then
+            deployment_automation_file="${DEPLOYMENT_AUTOMATION_PATH}/MYSQL_UBUNTU_IS_5_10"
+        elif [ "$database" = "mysql" ] && [ "$os" = "macos-latest" ]; then
+            deployment_automation_file="${DEPLOYMENT_AUTOMATION_PATH}/MYSQL_MAC_IS_5_10"
+        fi
+        ;;
+    "5.11.0")
+        if [ "$database" = "mysql" ] && [ "$os" = "ubuntu-latest" ]; then
+            deployment_automation_file="${DEPLOYMENT_AUTOMATION_PATH}/MYSQL_UBUNTU_IS_5_11"
+        elif [ "$database" = "mysql" ] && [ "$os" = "macos-latest" ]; then
+            deployment_automation_file="${DEPLOYMENT_AUTOMATION_PATH}/MYSQL_MAC_IS_5_11"
+        fi
+        ;;
+    "6.0.0")
+        if [ "$database" = "mysql" ] && [ "$os" = "ubuntu-latest" ]; then
+            deployment_automation_file="${DEPLOYMENT_AUTOMATION_PATH}/MYSQL_UBUNTU_IS_6_0"
+        elif [ "$database" = "mysql" ] && [ "$os" = "macos-latest" ]; then
+            deployment_automation_file="${DEPLOYMENT_AUTOMATION_PATH}/MYSQL_MAC_IS_6_0"
+        fi
+        ;;
+    "6.1.0")
+        if [ "$database" = "mysql" ] && [ "$os" = "ubuntu-latest" ]; then
+            deployment_automation_file="${DEPLOYMENT_AUTOMATION_PATH}/MYSQL_UBUNTU_IS_6_1"
+        elif [ "$database" = "mysql" ] && [ "$os" = "macos-latest" ]; then
+            deployment_automation_file="${DEPLOYMENT_AUTOMATION_PATH}/MYSQL_MAC_IS_6_1"
+        fi
+        ;;
+    "6.2.0")
+        if [ "$database" = "mysql" ] && [ "$os" = "ubuntu-latest" ]; then
+            deployment_automation_file="${DEPLOYMENT_AUTOMATION_PATH}/MYSQL_UBUNTU_IS_6_2"
+        elif [ "$database" = "mysql" ] && [ "$os" = "macos-latest" ]; then
+            deployment_automation_file="${DEPLOYMENT_AUTOMATION_PATH}/MYSQL_MAC_IS_6_2"
+        fi
+        ;;
+esac
 
-if [[ "$database" == "mysql" && "$os" == "ubuntu-latest" ]]; then
-    case "$currentVersion" in
-        "5.9.0" | "5.10.0" | "5.11.0" | "6.0.0" | "6.1.0" | "6.2.0")
-            deployment_file="$DEPLOYMENT_PATH/deployment.toml"
-            automation_file="$DEPLOYMENT_AUTOMATION_MYSQL_UBUNTU_IS_$currentVersion"
-            
-            if [[ -f "$automation_file" ]]; then
-                find "$deployment_file" -type f -name 'deployment.toml' -exec sh -c "cat '$automation_file' > {}" \;
-                echo "Deployment file for $currentVersion replaced successfully."
-            else
-                echo "Deployment automation file not found for version $currentVersion."
-            fi
-            ;;
-        *)
-            echo "Deployment file for version $currentVersion not supported."
-            ;;
-    esac
-fi
-
-if [[ "$database" == "mssql" && "$os" == "ubuntu-latest" ]]; then
-    case "$currentVersion" in
-        "5.9.0" | "5.10.0" | "5.11.0" | "6.0.0" | "6.1.0" | "6.2.0")
-            deployment_file="$DEPLOYMENT_PATH/deployment.toml"
-            automation_file="$DEPLOYMENT_AUTOMATION_MSSQL_UBUNTU_IS_$currentVersion"
-            
-            if [[ -f "$automation_file" ]]; then
-                find "$deployment_file" -type f -name 'deployment.toml' -exec sh -c "cat '$automation_file' > {}" \;
-                echo "Deployment file for $currentVersion replaced successfully."
-            else
-                echo "Deployment automation file not found for version $currentVersion."
-            fi
-            ;;
-        *)
-            echo "Deployment file for version $currentVersion not supported."
-            ;;
-    esac
+# Replace deployment file if automation file is available
+if [ -n "$deployment_automation_file" ]; then
+    find "$deployment_path" -type f -name 'deployment.toml' -exec sh -c "cat '$deployment_automation_file' > '{}'" \;
+    echo "Deployment file for $currentVersion replaced successfully."
 fi
