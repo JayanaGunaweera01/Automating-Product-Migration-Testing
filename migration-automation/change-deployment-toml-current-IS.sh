d68136cecc77d4db959cb66e51008b690f6a5f82
JayanaGunaweera01 <jayana.20200003@iit.ac.lk>
2023-05-22 21:35:12 +0530

    Did some minor changes

--- migration-automation/change-deployment-toml-current-IS.sh
+++ migration-automation/change-deployment-toml-current-IS.sh
@@ -164,5 +164,5 @@ fi
 # Replace deployment file if deployment automation file exists
 if [ -n "$deployment_automation_file" ]; then
     find "$deployment_path" -type f -name 'deployment.toml' -exec sh -c "cat '$deployment_automation_file' > '{}'" \;
-    echo "${GREEN}==> Did needed changes of deployment toml file of IS version "$1" relevant to "$2" database successfully.${RESET}"
+    echo "${GREEN}==> Did needed changes of deployment toml file of IS version "$currentVersion" relevant to "$database" database successfully.${RESET}"
 fi
