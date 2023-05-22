e5139a4ba7f2ff5d2c9b6f7cbf62b491485a7035
JayanaGunaweera01 <jayana.20200003@iit.ac.lk>
2023-05-22 14:05:18 +0530

    Did some minor changes

--- migration-automation/ubuntu-os/change-deployment-toml-ubuntu.sh
+++ migration-automation/ubuntu-os/change-deployment-toml-ubuntu.sh
@@ -16,7 +16,7 @@ version4="6.0.0"
 version5="6.1.0"
 version6="6.2.0"
 
-if os == "ubuntu-latest"; then
+if [ "$os" = "ubuntu-latest" ]; then
     deployment_file="$DEPLOYMENT_PATH/deployment.toml"
 
     # Source env file Ubuntu
@@ -25,7 +25,7 @@ if os == "ubuntu-latest"; then
     . ./env.sh
     echo "${GREEN}==> Env file for Ubuntu sourced successfully${RESET}"
 
-elif os" == "macos-latest"; then
+elif [ "$os" = "macos-latest" ]; then
     deployment_file="$DEPLOYMENT_PATH_MAC/deployment.toml"
 
     # Source env file Mac
@@ -37,23 +37,23 @@ fi
 replace_deployment_file() {
     local automation_file="$1"
 
-    if [[ -f "$automation_file" ]]; then
+    if [ -f "$automation_file" ]; then
         cp "$automation_file" "$deployment_file"
-        echo "${GREEN}==> Deployment toml file for WSO2 IS currentVersion and database database replaced successfully.${RESET}"
+        echo "${GREEN}==> Deployment toml file for WSO2 IS $currentVersion and $database database replaced successfully.${RESET}"
     else
-        echo "${GREEN}==> Deployment toml file not found for WSO2 IS currentVersion and database database.${RESET}"
+        echo "${GREEN}==> Deployment toml file not found for WSO2 IS $currentVersion and $database database.${RESET}"
     fi
 }
 
-if os == "ubuntu-latest" || os == "macos-latest" ; then
-    if database == "mysql" || database == "mssql" || database == "postgres"; then
-        case currentVersion in
-        version1 | version2 | version3 | version4 | version5 | version6)
-            version_with_underscores=currentVersion//./_
-            version_major_minor=version_with_underscores%_*
-            automation_file="$DEPLOYMENT_AUTOMATION_database^^_os^^_IS_$version_major_minor"
-            replace_deployment_file automation_file
-            ;;
+if [ "$os" = "ubuntu-latest" ] || [ "$os" = "macos-latest" ]; then
+    if [ "$database" = "mysql" ] || [ "$database" = "mssql" ] || [ "$database" = "postgres" ]; then
+        case "$currentVersion" in
+            $version1|$version2|$version3|$version4|$version5|$version6)
+                version_with_underscores="${currentVersion//./_}"
+                version_major_minor="${version_with_underscores%_*}"
+                automation_file="$DEPLOYMENT_AUTOMATION_${database^^}_${os^^}_IS_$version_major_minor"
+                replace_deployment_file "$automation_file"
+                ;;
         esac
     fi
 fi
