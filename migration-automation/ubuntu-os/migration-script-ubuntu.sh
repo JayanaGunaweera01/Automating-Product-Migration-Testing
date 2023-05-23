a8775ae8851d2d0aa8d917bdca8ac5476346e245
JayanaGunaweera01 <jayana.20200003@iit.ac.lk>
2023-05-23 17:09:33 +0530

    Did some minor changes

--- migration-automation/ubuntu-os/migration-script-ubuntu.sh
+++ migration-automation/ubuntu-os/migration-script-ubuntu.sh
@@ -147,7 +147,7 @@ echo "${GREEN}==> Identity server $3 started running!${RESET}"
 # Starting downloaded identity server
 #sh start-server-is-old.sh
 chmod +x start-server.sh
-sh start-server.sh "ubuntu-latest" "3"
+bash start-server.sh "ubuntu-latest" "3"
 
 cd "$AUTOMATION_HOME"
 
