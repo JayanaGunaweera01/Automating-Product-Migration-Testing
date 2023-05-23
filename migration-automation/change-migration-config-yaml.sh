8839b5e0e3c355afe8bc177af0cde1767fcd343f
JayanaGunaweera01 <jayana.20200003@iit.ac.lk>
2023-05-23 21:42:22 +0530

    Did some minor changes

--- migration-automation/change-migration-config-yaml.sh
+++ migration-automation/change-migration-config-yaml.sh
@@ -7,48 +7,34 @@ RESET='\033[0m'           # reset color
 # Get the value of the inputs
 currentVersion="$1"
 migratingVersion="$2"
+os="$3"
 
 # Setup file and path based on OS
 if [ "$os" = "ubuntu-latest" ]; then
   cd "/home/runner/work/Automating-Product-Migration-Testing/Automating-Product-Migration-Testing/migration-automation"
   chmod +x env.sh
-   . ./env.sh
+  . ./env.sh
   echo "${GREEN}==> Env file for Ubuntu sourced successfully${RESET}"
   cd "$MIGRATION_RESOURCES_NEW_IS"
-chmod +x "$MIGRATION_CONFIG_YAML"
+  chmod +x "$MIGRATION_CONFIG_YAML"
 
-for file in $(find "$MIGRATION_RESOURCES_NEW_IS" -type f -name 'migration-config.yaml');
-do
+  for file in $(find "$MIGRATION_RESOURCES_NEW_IS" -type f -name 'migration-config.yaml'); do
     sed -i "s/\(.*migrationEnable:.*\)/migrationEnable: \"true\"/" "$file"
     sed -i "s/\(.*currentVersion: .*\)/currentVersion: \"$currentVersion\"/" "$file"
     sed -i "s/\(.*migrateVersion: .*\)/migrateVersion: \"$migratingVersion\"/" "$file"
-done
+  done
 
 elif [ "$os" = "macos-latest" ]; then
-  cd "/Users/runner/work/Automating-Product-Migration-Testing/Automating-Product-Migration-Testing/migration-automation"  1
+  cd "/Users/runner/work/Automating-Product-Migration-Testing/Automating-Product-Migration-Testing/migration-automation" 1
   chmod +x env.sh
   source ./env.sh
   echo "${GREEN}==> Env file for Mac sourced successfully${RESET}"
- cd "$MIGRATION_RESOURCES_NEW_IS_MAC"
-chmod +x "$MIGRATION_CONFIG_YAML_MAC"
+  cd "$MIGRATION_RESOURCES_NEW_IS_MAC"
+  chmod +x "$MIGRATION_CONFIG_YAML_MAC"
 
-for file in $(find "$MIGRATION_RESOURCES_NEW_IS_MAC" -type f -name 'migration-config.yaml');
-do
+  for file in $(find "$MIGRATION_RESOURCES_NEW_IS_MAC" -type f -name 'migration-config.yaml'); do
     sed -i "s/\(.*migrationEnable:.*\)/migrationEnable: \"true\"/" "$file"
     sed -i "s/\(.*currentVersion: .*\)/currentVersion: \"$currentVersion\"/" "$file"
     sed -i "s/\(.*migrateVersion: .*\)/migrateVersion: \"$migratingVersion\"/" "$file"
-done
+  done
 fi
-
-
-cd "$MIGRATION_RESOURCES_NEW_IS"
-chmod +x "$MIGRATION_CONFIG_YAML"
-
-for file in $(find "$MIGRATION_RESOURCES_NEW_IS" -type f -name 'migration-config.yaml');
-do
-    sed -i "s/\(.*migrationEnable:.*\)/migrationEnable: \"true\"/" "$file"
-    sed -i "s/\(.*currentVersion: .*\)/currentVersion: \"$currentVersion\"/" "$file"
-    sed -i "s/\(.*migrateVersion: .*\)/migrateVersion: \"$migratingVersion\"/" "$file"
-done
-
-
