4bbbadd978bf852f083fa63596a345d88a759ba4
JayanaGunaweera01 <jayana.20200003@iit.ac.lk>
2023-05-23 19:11:54 +0530

    Did some minor changes

--- migration-automation/start-server.sh
+++ migration-automation/start-server.sh
@@ -50,35 +50,38 @@ elif [ "$os" = "macos-latest" ]; then
   fi
 fi
 
-echo "./wso2server.sh -Dcarbon.bootstrap.timeout=300" > start.sh
-chmod +x start.sh
-
-# Start the Identity Server in the background
-nohup ./start.sh >/dev/null 2>&1 &
+echo "./wso2server.sh -Dcarbon.bootstrap.timeout=300" >start.sh
+chmod +x start.sh && chmod 777 start.sh
+nohup ./start.sh &
 
 # Wait until server is up
 is_server_up() {
-  local status
-  status=$(curl -k -L -s -o /dev/null -w "%{http_code}" --request GET "https://localhost:9443/")
-  if "$status" -eq 200; then
-    return 0
-  fi
-  return 1
+    local status
+    status=$(curl -k -L -s \
+        -o /dev/null \
+        -w "%{http_code}" \
+        --request GET \
+        "https://localhost:9443/")
+    if [ "$status" -eq 200 ]; then
+        return 0
+    fi
+    return 1
 }
 
 wait_until_server_is_up() {
-  local timeout=600
-  local wait_time=0
-  while ! is_server_up; do
-    echo "Waiting until the server starts..."
-    sleep 10
-    wait_time=$((wait_time + 10))
-    if [ "$wait_time" -ge "$timeout" ]; then
-      echo "Timeout: The server did not start within $timeout seconds"
-      exit 1
-    fi
-  done
+    local timeout=600
+    local wait_time=0
+    while ! is_server_up; do
+        echo "Waiting until server starts..." &&
+            sleep 10
+        wait_time=$((wait_time + 10))
+        if [ "$wait_time" -ge "$timeout" ]; then
+            echo "Timeout: server did not start within $timeout seconds"
+            exit 1
+        fi
+    done
 }
 
 wait_until_server_is_up
 echo "${GREEN}WSO2 Identity Server has started successfully${RESET}"
+
