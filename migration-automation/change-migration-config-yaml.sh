5b8c8b214a7491a125896360cc0da3453296be38
Jayana  Gunaweera <79576139+JayanaGunaweera01@users.noreply.github.com>
2023-05-25 22:25:51 +0530

    Update change-migration-config-yaml.sh

--- migration-automation/change-migration-config-yaml.sh
+++ migration-automation/change-migration-config-yaml.sh
@@ -17,14 +17,14 @@ if [ "$os" = "ubuntu-latest" ]; then
   echo -e "${GREEN}==> Env file for Ubuntu sourced successfully${RESET}"
   cd "$MIGRATION_RESOURCES_NEW_IS"
   chmod +x "$MIGRATION_CONFIG_YAML"
-
-  for file in $(find "$MIGRATION_RESOURCES_NEW_IS" -type f -name 'migration-config.yaml'); do
+  
+  for file in $(find "$MIGRATION_RESOURCES_NEW_IS_MAC" -type f -name 'migration-config.yaml'); do
     sed -i "s/\(.*migrationEnable:.*\)/migrationEnable: \"true\"/" "$file"
     sed -i "s/\(.*currentVersion: .*\)/currentVersion: \"$currentVersion\"/" "$file"
     sed -i "s/\(.*migrateVersion: .*\)/migrateVersion: \"$migratingVersion\"/" "$file"
-    
-    # Define the search pattern for the block of text   
-    if [ "$currentVersion" = "5.9.0" || "$currentVersion" = "5.10.0" || "$currentVersion" = "5.11.0" ] && [ "$migratingVersion" = "6.0.0" ]; then  
+  
+    # Define the search pattern for the block of text 
+    if [ "$currentVersion" = "5.9.0" || "$currentVersion" = "5.10.0" || "$currentVersion" = "5.11.0" || "$currentVersion" = "6.0.0" || "$currentVersion" = "6.1.0" || "$currentVersion" = "6.2.0" ] && [ "$migratingVersion" = "6.0.0" || "$migratingVersion" = "6.1.0" || "$migratingVersion" = "6.2.0" ]; then  
       search_pattern='version: "5.11.0"\n   migratorConfigs:\n   -\n     name: "EncryptionAdminFlowMigrator"\n     order: 1\n     parameters:\n       currentEncryptionAlgorithm: "RSA/ECB/OAEPwithSHA1andMGF1Padding"\n       migratedEncryptionAlgorithm: "AES/GCM/NoPadding"\n       schema: "identity"'
 
       # Define the replacement line
@@ -32,13 +32,13 @@ if [ "$os" = "ubuntu-latest" ]; then
 
       # Find and replace the line within the block of text
       sed -i "/$search_pattern/{n;n;n;n;n;s/.*/$replacement_line/}" "$file"
-      echo "${GREEN}==> CurrentEncryptionAlgorithm changed to \"RSA\" which is a special migration config change when migrating to IS 6.0.0${RESET}"
+      echo "${GREEN}==> CurrentEncryptionAlgorithm changed to \"RSA\" which is a special migration config change when migrating to to versions above IS 5.11.0${RESET}"
     fi
     
     # Check conditions to modify transformToSymmetric (This is a special migration config change when migrating to IS 5.11.0)
-    if [ "$currentVersion" = "5.9.0" || "$currentVersion" = "5.10.0" || "$currentVersion" = "5.11.0" ] && [ "$migratingVersion" = "5.11.0" || "$migratingVersion" = "6.0.0" ]; then
+    if [ "$currentVersion" = "5.9.0" || "$currentVersion" = "5.10.0" || "$currentVersion" = "5.11.0" || "$currentVersion" = "6.0.0" || "$currentVersion" = "6.1.0" || "$currentVersion" = "6.2.0" ] && [ "$migratingVersion" = "5.11.0" || "$migratingVersion" = "6.0.0" || "$migratingVersion" = "6.1.0" || "$migratingVersion" = "6.2.0" ]; then
       sed -i 's/transformToSymmetric:.*/transformToSymmetric: "true"/' "$file"
-      echo "${GREEN}==> Value of transformToSymmetric changed to true in migration-config.yaml which is a special migration config change when migrating to IS 5.11.0${RESET}"
+      echo "${GREEN}==> Value of transformToSymmetric changed to true in migration-config.yaml which is a special migration config change when migrating to versions above IS 5.11.0${RESET}"
     fi
   done
 
@@ -56,7 +56,7 @@ elif [ "$os" = "macos-latest" ]; then
     sed -i "s/\(.*migrateVersion: .*\)/migrateVersion: \"$migratingVersion\"/" "$file"
   
     # Define the search pattern for the block of text 
-    if [ "$currentVersion" = "5.9.0" || "$currentVersion" = "5.10.0" || "$currentVersion" = "5.11.0" ] && [ "$migratingVersion" = "6.0.0" ]; then  
+    if [ "$currentVersion" = "5.9.0" || "$currentVersion" = "5.10.0" || "$currentVersion" = "5.11.0" || "$currentVersion" = "6.0.0" || "$currentVersion" = "6.1.0" || "$currentVersion" = "6.2.0" ] && [ "$migratingVersion" = "6.0.0" || "$migratingVersion" = "6.1.0" || "$migratingVersion" = "6.2.0" ]; then  
       search_pattern='version: "5.11.0"\n   migratorConfigs:\n   -\n     name: "EncryptionAdminFlowMigrator"\n     order: 1\n     parameters:\n       currentEncryptionAlgorithm: "RSA/ECB/OAEPwithSHA1andMGF1Padding"\n       migratedEncryptionAlgorithm: "AES/GCM/NoPadding"\n       schema: "identity"'
 
       # Define the replacement line
@@ -64,13 +64,13 @@ elif [ "$os" = "macos-latest" ]; then
 
       # Find and replace the line within the block of text
       sed -i "/$search_pattern/{n;n;n;n;n;s/.*/$replacement_line/}" "$file"
-      echo "${GREEN}==> CurrentEncryptionAlgorithm changed to \"RSA\" which is a special migration config change when migrating to IS 6.0.0${RESET}"
+      echo "${GREEN}==> CurrentEncryptionAlgorithm changed to \"RSA\" which is a special migration config change when migrating to to versions above IS 5.11.0${RESET}"
     fi
     
     # Check conditions to modify transformToSymmetric (This is a special migration config change when migrating to IS 5.11.0)
-    if [ "$currentVersion" = "5.9.0" || "$currentVersion" = "5.10.0" || "$currentVersion" = "5.11.0" ] && [ "$migratingVersion" = "5.11.0" || "$migratingVersion" = "6.0.0" ]; then
+    if [ "$currentVersion" = "5.9.0" || "$currentVersion" = "5.10.0" || "$currentVersion" = "5.11.0" || "$currentVersion" = "6.0.0" || "$currentVersion" = "6.1.0" || "$currentVersion" = "6.2.0" ] && [ "$migratingVersion" = "5.11.0" || "$migratingVersion" = "6.0.0" || "$migratingVersion" = "6.1.0" || "$migratingVersion" = "6.2.0" ]; then
       sed -i 's/transformToSymmetric:.*/transformToSymmetric: "true"/' "$file"
-      echo "${GREEN}==> Value of transformToSymmetric changed to true in migration-config.yaml which is a special migration config change when migrating to IS 5.11.0${RESET}"
+      echo "${GREEN}==> Value of transformToSymmetric changed to true in migration-config.yaml which is a special migration config change when migrating to versions above IS 5.11.0${RESET}"
     fi
   done
 fi
