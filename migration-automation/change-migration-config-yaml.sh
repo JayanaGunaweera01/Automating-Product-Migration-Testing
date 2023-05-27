af04f400aa627e7e7fe8a6af3007a8e95df71198
JayanaGunaweera01 <jayana.20200003@iit.ac.lk>
2023-05-27 17:46:26 +0530

    Did some minor changes

--- migration-automation/change-migration-config-yaml.sh
+++ migration-automation/change-migration-config-yaml.sh
@@ -9,6 +9,17 @@ currentVersion="$1"
 migratingVersion="$2"
 os="$3"
 
+#!/bin/bash
+
+# Define color variables
+GREEN='\033[0;32m\033[1m' # green color
+RESET='\033[0m'           # reset color
+
+# Get the value of the inputs
+currentVersion="$1"
+migratingVersion="$2"
+os="$3"
+
 # Setup file and path based on OS
 if [ "$os" = "ubuntu-latest" ]; then
   cd "/home/runner/work/Automating-Product-Migration-Testing/Automating-Product-Migration-Testing/migration-automation"
@@ -34,7 +45,7 @@ if [ "$os" = "ubuntu-latest" ]; then
       replacement_line='       currentEncryptionAlgorithm: "RSA"'
 
       # Find and replace the line within the block of text
-      sed -i "/$search_pattern/{n;n;n;n;n;s/.*/$replacement_line/}" "$file"
+      sed -i "s~$search_pattern~$replacement_line~" "$file"
     done
     echo "${GREEN}==> CurrentEncryptionAlgorithm changed to \"RSA\" which is a special migration config change when migrating to versions above IS 5.11.0${RESET}"
   fi
@@ -42,7 +53,7 @@ if [ "$os" = "ubuntu-latest" ]; then
   # Check conditions to modify transformToSymmetric (This is a special migration config change when migrating to IS 5.11.0)
   if [ "$migratingVersion" = "5.11.0" ] || [ "$migratingVersion" = "6.0.0" ] || [ "$migratingVersion" = "6.1.0" ] || [ "$migratingVersion" = "6.2.0" ]; then
     for file in $(find "$MIGRATION_RESOURCES_NEW_IS_UBUNTU" -type f -name 'migration-config.yaml'); do
-      sed -i 's/transformToSymmetric:.*/transformToSymmetric: "true"/' "$file"
+      sed -i 's~transformToSymmetric:.*~transformToSymmetric: "true"~' "$file"
     done
     echo "${GREEN}==> Value of transformToSymmetric changed to true in migration-config.yaml which is a special migration config change when migrating to versions above IS 5.11.0${RESET}"
   fi
