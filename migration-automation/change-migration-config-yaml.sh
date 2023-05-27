1e80db87fbea313ae1268ebae9137bd179e68a22
JayanaGunaweera01 <jayana.20200003@iit.ac.lk>
2023-05-27 17:51:55 +0530

    Did some minor changes

--- migration-automation/change-migration-config-yaml.sh
+++ migration-automation/change-migration-config-yaml.sh
@@ -59,7 +59,7 @@ if [ "$os" = "ubuntu-latest" ]; then
   fi
 fi
 
-elif [ "$os" = "macos-latest" ]; then
+if [ "$os" = "macos-latest" ]; then
   cd "/Users/runner/work/Automating-Product-Migration-Testing/Automating-Product-Migration-Testing/migration-automation"
   chmod +x env.sh
   source ./env.sh
@@ -71,23 +71,28 @@ elif [ "$os" = "macos-latest" ]; then
     sed -i "s/\(.*migrationEnable:.*\)/migrationEnable: \"true\"/" "$file"
     sed -i "s/\(.*currentVersion: .*\)/currentVersion: \"$currentVersion\"/" "$file"
     sed -i "s/\(.*migrateVersion: .*\)/migrateVersion: \"$migratingVersion\"/" "$file"
+    echo "${GREEN}==> Versions Changed.${RESET}"
+  done
 
-    # Define the search pattern for the block of text
-    if ["$migratingVersion" = "6.0.0" || "$migratingVersion" = "6.1.0" || "$migratingVersion" = "6.2.0" ]; then
+  # Define the search pattern for the block of text
+  if [ "$migratingVersion" = "6.0.0" ] || [ "$migratingVersion" = "6.1.0" ] || [ "$migratingVersion" = "6.2.0" ]; then
+    for file in $(find "$MIGRATION_RESOURCES_NEW_IS_MAC" -type f -name 'migration-config.yaml'); do
       search_pattern='version: "5.11.0"\n   migratorConfigs:\n   -\n     name: "EncryptionAdminFlowMigrator"\n     order: 1\n     parameters:\n       currentEncryptionAlgorithm: "RSA/ECB/OAEPwithSHA1andMGF1Padding"\n       migratedEncryptionAlgorithm: "AES/GCM/NoPadding"\n       schema: "identity"'
 
       # Define the replacement line
       replacement_line='       currentEncryptionAlgorithm: "RSA"'
 
       # Find and replace the line within the block of text
-      sed -i "/$search_pattern/{n;n;n;n;n;s/.*/$replacement_line/}" "$file"
-      echo "${GREEN}==> CurrentEncryptionAlgorithm changed to \"RSA\" which is a special migration config change when migrating to to versions above IS 5.11.0${RESET}"
-    fi
+      sed -i "s~$search_pattern~$replacement_line~" "$file"
+    done
+    echo "${GREEN}==> CurrentEncryptionAlgorithm changed to \"RSA\" which is a special migration config change when migrating to versions above IS 5.11.0${RESET}"
+  fi
 
-    # Check conditions to modify transformToSymmetric (This is a special migration config change when migrating to IS 5.11.0)
-    if [ "$migratingVersion" = "5.11.0" || "$migratingVersion" = "6.0.0" || "$migratingVersion" = "6.1.0" || "$migratingVersion" = "6.2.0" ]; then
-      sed -i 's/transformToSymmetric:.*/transformToSymmetric: "true"/' "$file"
-      echo "${GREEN}==> Value of transformToSymmetric changed to true in migration-config.yaml which is a special migration config change when migrating to versions above IS 5.11.0${RESET}"
-    fi
-  done
+  # Check conditions to modify transformToSymmetric (This is a special migration config change when migrating to IS 5.11.0)
+  if [ "$migratingVersion" = "5.11.0" ] || [ "$migratingVersion" = "6.0.0" ] || [ "$migratingVersion" = "6.1.0" ] || [ "$migratingVersion" = "6.2.0" ]; then
+    for file in $(find "$MIGRATION_RESOURCES_NEW_IS_MAC" -type f -name 'migration-config.yaml'); do
+      sed -i 's~transformToSymmetric:.*~transformToSymmetric: "true"~' "$file"
+    done
+    echo "${GREEN}==> Value of transformToSymmetric changed to true in migration-config.yaml which is a special migration config change when migrating to versions above IS 5.11.0${RESET}"
+  fi
 fi
