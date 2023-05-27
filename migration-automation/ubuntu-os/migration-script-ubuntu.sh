02f6620e6e8883901038924d531e3fd0116ef323
JayanaGunaweera01 <jayana.20200003@iit.ac.lk>
2023-05-27 20:41:40 +0530

    Did some minor changes

--- migration-automation/ubuntu-os/migration-script-ubuntu.sh
+++ migration-automation/ubuntu-os/migration-script-ubuntu.sh
@@ -264,7 +264,8 @@ chmod +x change-deployment-toml.sh
 sh change-deployment-toml.sh "$3" "$4" "$5" "$6" "4"
 echo "${GREEN}==> Deployment.toml changed successfully${RESET}"
 echo "${BLUE}==> Copied deployment toml of $3 to $4 successfully!${RESET}"
-
+wait $!
+ 
 # Execute consent management db scripts for IS 5.11.0 - MySQL
 if [ "$4" = "5.11.0" && "$5" = "mysql" ]; then
     docker exec -i amazing_feynman sh -c 'exec mysql -uroot -proot -D mydb' </home/runner/work/Automating-Product-Migration-Testing/Automating-Product-Migration-Testing/utils/other-db-scripts/config-management-is-5-11.sql
