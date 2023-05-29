54fe0a7e7ca6ff5622eea2c19478f96aba8db8c3
JayanaGunaweera01 <jayana.20200003@iit.ac.lk>
2023-05-30 04:18:31 +0530

    Did some minor changes

--- migration-automation/ubuntu-os/migration-script-ubuntu.sh
+++ migration-automation/ubuntu-os/migration-script-ubuntu.sh
@@ -397,7 +397,7 @@ cd "$SERVICE_PROVIDER_PATH"
 echo "${GREEN}==> Entered to data population directory-service provider creation${RESET}"
 
 # Run data-population-script.sh which is capable of populating data to create users,tenants,userstores,generate tokens etc.
-sh generate-oauth-token-linux.sh
+#sh generate-oauth-token-linux.sh
 sh get-oauth-token.sh
 wait $!
 echo "${GREEN}==> Validated database successfully${RESET}"
