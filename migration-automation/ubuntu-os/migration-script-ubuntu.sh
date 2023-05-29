ef361f9e1cfab957a4b7988444c2b873f36df5ba
JayanaGunaweera01 <jayana.20200003@iit.ac.lk>
2023-05-29 20:55:37 +0530

    Did some minor changes

--- migration-automation/ubuntu-os/migration-script-ubuntu.sh
+++ migration-automation/ubuntu-os/migration-script-ubuntu.sh
@@ -305,8 +305,42 @@ print_star_line
 echo "${GREEN}==> Started running migration client${RESET}"
 
 # Start the migration server
-chmod +x start-server.sh
-sh start-server.sh "$6" "3" "$3" "$4" "true"
+#chmod +x start-server.sh
+#sh start-server.sh "$6" "3" "$3" "$4" "true"
+# Start the migration server
+echo "./wso2server.sh -Dmigrate -Dcomponent=identity -Dcarbon.bootstrap.timeout=300" >start.sh
+chmod +x start.sh && chmod 777 start.sh
+nohup ./start.sh &
+
+# Wait until server is up
+is_server_up() {
+    local status
+    status=$(curl -k -L -s \
+        -o /dev/null \
+        -w "%{http_code}" \
+        --request GET \
+        "https://localhost:9443/")
+    if [ "$status" -eq 200 ]; then
+        return 0
+    fi
+    return 1
+}
+
+wait_until_server_is_up() {
+    local timeout=600
+    local wait_time=0
+    while ! is_server_up; do
+        echo "Migration is currently in progress. Please wait..." &&
+            sleep 10
+        wait_time=$((wait_time + 10))
+        if [ "$wait_time" -ge "$timeout" ]; then
+            echo "Timeout: migration did not complete within $timeout seconds"
+            exit 1
+        fi
+    done
+}
+
+wait_until_server_is_up
 echo "${GREEN}==> Yay! Migration process completed!🎉 Check artifacts after completing workflow run to check whether there are any errors${RESET}"
 
 # Stop wso2IS migration server
