b2b497961409c51de25d7ade54b2515f92c38460
Jayana  Gunaweera <79576139+JayanaGunaweera01@users.noreply.github.com>
2023-05-31 05:09:19 +0530

    Update change-migration-config-yaml.sh

--- migration-automation/change-migration-config-yaml.sh
+++ migration-automation/change-migration-config-yaml.sh
@@ -82,12 +82,12 @@ if [ "$os" = "macos-latest" ]; then
   cd "$MIGRATION_RESOURCES_NEW_IS_MAC"
   chmod +x "$MIGRATION_CONFIG_YAML_MAC"
 
-  for file in $(find "$MIGRATION_RESOURCES_NEW_IS_MAC" -type f -name 'migration-config.yaml'); do
-    sed -i "s/\(.*migrationEnable:.*\)/migrationEnable: \"true\"/" "$file"
-    sed -i "s/\(.*currentVersion: .*\)/currentVersion: \"$currentVersion\"/" "$file"
-    sed -i "s/\(.*migrateVersion: .*\)/migrateVersion: \"$migratingVersion\"/" "$file"
-    echo "${GREEN}==> Versions Changed.${RESET}"
-  done
+ for file in $(find "$MIGRATION_RESOURCES_NEW_IS_MAC" -type f -name 'migration-config.yaml'); do
+  sed -i "" "s/\(.*migrationEnable:.*\)/migrationEnable: \"true\"/" "$file"
+  sed -i "" "s/\(.*currentVersion: .*\)/currentVersion: \"$currentVersion\"/" "$file"
+  sed -i "" "s/\(.*migrateVersion: .*\)/migrateVersion: \"$migratingVersion\"/" "$file"
+  echo "${GREEN}==> Versions Changed.${RESET}"
+done
 
   # Define the search pattern for the block of text
   if [ "$migratingVersion" = "6.0.0" ] || [ "$migratingVersion" = "6.1.0" ] || [ "$migratingVersion" = "6.2.0" ]; then
@@ -101,7 +101,7 @@ if [ "$os" = "macos-latest" ]; then
       replacement_line='       currentEncryptionAlgorithm: "RSA"'
       for file in $(find "$MIGRATION_RESOURCES_NEW_IS_MAC" -type f -name 'migration-config.yaml'); do
         # Find and replace the line within the block of text
-        sed -i "s~$search_pattern~$replacement_line~" "$file"
+        sed -i "" "s~$search_pattern~$replacement_line~" "$file"
       done
     done
     echo "${GREEN}==> CurrentEncryptionAlgorithm changed to \"RSA\" which is a special migration config change when migrating to versions above IS 5.11.0${RESET}"
@@ -112,12 +112,11 @@ if [ "$os" = "macos-latest" ]; then
     cd "$MIGRATION_RESOURCES_NEW_IS_MAC"
     chmod +x migration-config.yaml
     for file in $(find "$MIGRATION_RESOURCES_NEW_IS_MAC" -type f -name 'migration-config.yaml'); do
-      sed -i 's~transformToSymmetric:.*~transformToSymmetric: "true"~' "$file"
+      sed -i "" 's~transformToSymmetric:.*~transformToSymmetric: "true"~' "$file"
       echo "${GREEN}==> Value of transformToSymmetric changed to true in migration-config.yaml which is a special migration config change when migrating to versions above IS 5.11.0${RESET}"
       echo "Content of migration-config-yaml file:"
       cat "migration-config.yaml"
       echo "${GREEN}==> Did all the needed changes to migration-config.yaml  successfully.${RESET}"
-
     done
 
   fi
