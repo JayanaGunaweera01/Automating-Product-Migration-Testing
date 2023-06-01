c910072fb9c352b20c8f9d862a5364661bb0fb57
JayanaGunaweera01 <jayana.20200003@iit.ac.lk>
2023-06-01 11:21:07 +0530

    Did some minor changes

--- migration-automation/mac-os/migration-script-mac.sh
+++ migration-automation/mac-os/migration-script-mac.sh
@@ -141,6 +141,8 @@ wait $!
 echo "${GREEN}==> Entered to Management console home page successfully${RESET}"
 
 cd "$DATA_POPULATION_MAC"
+pwd
+ls -a
 echo "${GREEN}==> Entered the data population directory successfully.${RESET}"
 
 # Run data-population-script.sh which is capable of populating data to create users,tenants,userstores,generate tokens etc.
