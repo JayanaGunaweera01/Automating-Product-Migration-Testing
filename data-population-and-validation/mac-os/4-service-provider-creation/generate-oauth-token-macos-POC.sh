800e2cbdc3c87a0867d9b140d94bb7bce7fa6614
JayanaGunaweera01 <jayana.20200003@iit.ac.lk>
2023-06-02 01:37:42 +0530

    Did some minor changes

--- data-population-and-validation/mac-os/4-service-provider-creation/generate-oauth-token-macos-POC.sh
+++ data-population-and-validation/mac-os/4-service-provider-creation/generate-oauth-token-macos-POC.sh
@@ -21,7 +21,7 @@ fi
 base64_encoded=$(echo -n "$client_id:$client_secret" | base64)
 
 # Get access token
-echo -e "${YELLOW}Getting access token...${NC}"
+echo "${YELLOW}Getting access token...${NC}"
 token_response=$(curl -ks -X POST https://localhost:9443/oauth2/token \
   -H "Authorization: Basic $base64_encoded" \
   -H 'Content-Type: application/x-www-form-urlencoded' \
@@ -34,27 +34,27 @@ refresh_token=$(echo $token_response | jq -r '.refresh_token')
 if [ -n "$access_token" ] && [ -n "$refresh_token" ]; then
   # Print access token and refresh token
   if [ "$access_token" != "null" ]; then
-    echo -e "Access token: \033[31m$access_token\033[0m"
+    echo "Access token: \033[31m$access_token\033[0m"
   fi
   if [ "$refresh_token" != "null" ]; then
     echo "Refresh token: \033[31m$refresh_token\033[0m"
   fi
 
   # Store access token and refresh token in a file
-  if grep -q "access_token" client_credentials; then
-    sed -i "s/access_token=.*/access_token=$access_token/" client_credentials
+  if grep -q "access_token" "$script_dir/client_credentials"; then
+    sed -i '' "s/access_token=.*/access_token=$access_token/" "$script_dir/client_credentials"
   else
-    echo "access_token=$access_token" >> client_credentials
+    echo "access_token=$access_token" >> "$script_dir/client_credentials"
   fi
 
-  if grep -q "refresh_token" client_credentials; then
-    sed -i "s/refresh_token=.*/refresh_token=$refresh_token/" client_credentials
+  if grep -q "refresh_token" "$script_dir/client_credentials"; then
+    sed -i '' "s/refresh_token=.*/refresh_token=$refresh_token/" "$script_dir/client_credentials"
   else
-    echo "refresh_token=$refresh_token" >> client_credentials
+    echo "refresh_token=$refresh_token" >> "$script_dir/client_credentials"
   fi
 
   # Print success message
-  echo -e "${GREEN}An access token and a refresh token generated successfully.${NC}"
+  echo "${GREEN}An access token and a refresh token generated successfully.${NC}"
 else
-  echo -e "${GREEN}Database validation failed.${NC}"
+  echo "${GREEN}Database validation failed.${NC}"
 fi
