d43e6de1fa09f2c34520dd4a738ccf6ef3d99c59
JayanaGunaweera01 <jayana.20200003@iit.ac.lk>
2023-06-02 05:00:58 +0530

    Did some minor changes

--- data-population-and-validation/mac-os/4-service-provider-creation/generate-oauth-token-macos-POC.sh
+++ data-population-and-validation/mac-os/4-service-provider-creation/generate-oauth-token-macos-POC.sh
@@ -11,7 +11,7 @@ script_dir="/Users/runner/work/Automating-Product-Migration-Testing/Automating-P
 
 # Load client_id and client_secret from file
 if [ -f "$script_dir/client_credentials" ]; then
-   source "$script_dir/client_credentials"
+   . "$script_dir/client_credentials"
 else
   echo "${RED}Error: client_credentials file not found.${NC}"
   exit 1
