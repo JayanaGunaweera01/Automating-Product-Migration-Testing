5f7c451a0de53ef78601709c57c60a6eb31f035f
JayanaGunaweera01 <jayana.20200003@iit.ac.lk>
2023-06-02 00:05:21 +0530

    Did some minor changes

--- data-population-and-validation/ubuntu-os/automated-data-population-and-validation-script.sh
+++ data-population-and-validation/ubuntu-os/automated-data-population-and-validation-script.sh
@@ -15,33 +15,34 @@ if [ -d "$data_population_dir" ]; then
     exit 1
   }
 
-  # execute scripts in order
-  for script in \
-    "1-user-creation/create-user.sh" \
-    "1-user-creation/create-bulk-users.sh" \
-    "2-tenant-creation/create-tenant.sh" \
-    "2-tenant-creation/register-an-app-in-a-tenant.sh" \
-    "2-tenant-creation/get-access-token-tenantwise.sh" \
-    "3-userstore-creation/create-userstore.sh" \
-    "3-userstore-creation/create-user-in-userstore.sh" \
-    "4-service-provider-creation/register-a-service-provider.sh" \
-    "4-service-provider-creation/create-user-in-a-service-provider.sh" \
-    "4-service-provider-creation/get-oauth-token.sh" \
-    "5-group-creation/create-group.sh" \
-    "5-group-creation/create-groups-with-users.sh"; do
-    # construct the full path of the script
-    script_path="$data_population_dir/$script"
+# execute scripts in order
+for script in \
+  "1-user-creation/create-user.sh" \
+  "1-user-creation/create-bulk-users.sh" \
+  "2-tenant-creation/create-tenant.sh" \
+  "2-tenant-creation/register-an-app-in-a-tenant.sh" \
+  "2-tenant-creation/get-access-token-tenantwise.sh" \
+  "3-userstore-creation/create-userstore.sh" \
+  "3-userstore-creation/create-user-in-userstore.sh" \
+  "4-service-provider-creation/register-a-service-provider.sh" \
+  "4-service-provider-creation/create-user-in-a-service-provider.sh" \
+  "4-service-provider-creation/get-oauth-token.sh" \
+  "5-group-creation/create-group.sh" \
+  "5-group-creation/create-groups-with-users.sh"; do
+  # construct the full path of the script
+  script_path="$data_population_dir/$script"
 
-    # check if script exists and is executable
-    if [ -f "$script_path" ]; then
-      chmod +x "$script_path" # make the script executable
-      printf "${ORANGE}Running script: %s${RESET}\n" "$script_path"
-      # execute script and redirect output to stdout
-      "$script_path" >&1
-    else
-      echo "${GREEN}==> Script '$script_path' does not exist.${RESET}"
-    fi
-  done
+  # check if script exists and is executable
+  if [ -f "$script_path" ]; then
+    chmod +x "$script_path" # make the script executable
+    printf "${ORANGE}Running script: %s${RESET}\n" "$script_path"
+    # execute script and redirect output to stdout and client_credentials file
+    "$script_path" | tee -a client_credentials
+  else
+    echo "${GREEN}==> Script '$script_path' does not exist.${RESET}"
+  fi
+done
+
 else
 
   echo "${GREEN}==> Directory '$data_population_dir' does not exist.${RESET}"
