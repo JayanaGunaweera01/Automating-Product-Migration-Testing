71f0e0a55f15355260b4ea2d7d14e91e46b26d35
JayanaGunaweera01 <jayana.20200003@iit.ac.lk>
2023-06-03 04:06:32 +0530

    Did some minor changes

--- data-population-and-validation/ubuntu-os/4-service-provider-creation/generate-oauth-token-linux.sh
+++ data-population-and-validation/ubuntu-os/4-service-provider-creation/generate-oauth-token-linux.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 # Define colors
@@ -11,8 +12,7 @@ script_dir="$(dirname "$(realpath "$0")")"
 
 # Load client_id and client_secret from file
 if [ -f "$script_dir/client_credentials" ]; then
-  . "$script_dir/client_credentials"
-  echo "${GREEN}Sourced client_credentials file.${NC}"
+   . "$script_dir/client_credentials"
 else
   echo "${RED}Error: client_credentials file not found.${NC}"
   exit 1
@@ -29,25 +29,16 @@ token_response=$(curl -ks -X POST https://localhost:9443/oauth2/token \
   -d 'grant_type=password&username=admin&password=admin&scope=somescope_password')
 
 # Extract access token and refresh token from response
-access_token=$(echo "$token_response" | jq -r '.access_token')
-refresh_token=$(echo "$token_response" | jq -r '.refresh_token')
+access_token=$(echo $token_response | jq -r '.access_token')
+refresh_token=$(echo $token_response | jq -r '.refresh_token')
 
-# Check if access_token and refresh_token are present
 if [ -n "$access_token" ] && [ -n "$refresh_token" ]; then
-  # Print access token and refresh token if found
+  # Print access token and refresh token
   if [ "$access_token" != "null" ]; then
-    echo -e "Access token: \033[31m$access_token\033[0m"
-    access_token_found=true
-  else
-    echo "No access token found."
-    access_token_found=false
+    echo "Access token: \033[31m$access_token\033[0m"
   fi
   if [ "$refresh_token" != "null" ]; then
-    echo -e "Refresh token: \033[31m$refresh_token\033[0m"
-    refresh_token_found=true
-  else
-    echo "No refresh token found."
-    refresh_token_found=false
+    echo "Refresh token: \033[31m$refresh_token\033[0m"
   fi
 
   # Store access token and refresh token in a file
@@ -63,10 +54,8 @@ if [ -n "$access_token" ] && [ -n "$refresh_token" ]; then
     echo "refresh_token=$refresh_token" >> client_credentials
   fi
 
-  # Check if both access_token and refresh_token are found, and print success message
-  if [ "$access_token_found" = true ] && [ "$refresh_token_found" = true ]; then
-    echo "${GREEN}An access token and a refresh token generated successfully.${NC}"
-  fi
+  # Print success message
+  echo "${GREEN}An access token and a refresh token generated successfully.${NC}"
 else
   echo "${GREEN}Database validation failed.${NC}"
 fi
