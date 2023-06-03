cf15027b22eaf7583873c9249c57274b7c2bc191
Jayana  Gunaweera <79576139+JayanaGunaweera01@users.noreply.github.com>
2023-06-03 22:09:35 +0530

    Update migration-script-mac.sh

--- migration-automation/mac-os/migration-script-mac.sh
+++ migration-automation/mac-os/migration-script-mac.sh
@@ -253,7 +253,7 @@ echo "${BLUE}==> Copied userstores, tenants,jar files,.jks files from oldIS to n
 
 # Deployment toml changes in new is version
 chmod +x change-deployment-toml.sh
-sh change-deployment-toml.sh "$currentVersion" "$migratingVersion" "$database" "$os" "migration"
+sh change-deployment-toml.sh "$currentVersion" "$migratingVersion" "$database" "$os" "migrated"
 echo "${GREEN}==> Deployment.toml changed successfully${RESET}"
 echo "${BLUE}==> Copied deployment toml of "$currentVersion" to "$migratingVersion" successfully!${RESET}"
 
