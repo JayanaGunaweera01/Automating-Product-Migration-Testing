046fc42ef0f28b992e73988c060a76880f3cc736
JayanaGunaweera01 <jayana.20200003@iit.ac.lk>
2023-06-04 18:34:28 +0530

    Did some minor changes

--- data-population-and-validation/ubuntu-os/3-userstore-creation/create-user-in-userstore.sh
+++ data-population-and-validation/ubuntu-os/3-userstore-creation/create-user-in-userstore.sh
@@ -3,13 +3,15 @@
 # Define colors
 RED='\033[0;31m'
 GREEN='\033[1;38;5;206m'
-YELLOW='\033[0;33m'
 PURPLE='\033[1;35m'
 BOLD='\033[1m'
 NC='\033[0m' # No Color
 
+# Set the userstore name
+USERSTORE_NAME="$USERSTORE_NAME"
+
 # Create the user in the userstore
-response=$(curl -k --location --request POST "$SCIM_USER_EP_USERSTORE" \
+response=$(curl -k --location --request POST "https://localhost:9443/t/carbon.super/api/server/v1/userstores/$USERSTORE_NAME/users" \
     --header 'Content-Type: application/json' \
     --header 'Authorization: Basic YWRtaW46YWRtaW4=' \
     --data-raw '{
@@ -30,20 +32,20 @@ response=$(curl -k --location --request POST "$SCIM_USER_EP_USERSTORE" \
     }')
 
 # Check if the response contains any error message
-if echo "$response" | grep -q '"schemas":'; then
+if [[ "$response" == *"schemas"* ]]; then
     # If there is no error, print the success message
-    echo -e "${GREEN}${BOLD}A user has been created in the userstore. User name=${USERSTORE_USER_NAME}, Group name=${USERSTORE_GROUP_NAME}${NC}"
+    echo -e "${GREEN}${BOLD}A user has been created in the userstore '$USERSTORE_NAME'. User name=${USERSTORE_USER_NAME}, Group name=${USERSTORE_GROUP_NAME}${NC}"
     # Print the additional details
     echo "Additional Details:"
-    echo "User Name: ${PURPLE}$USERSTORE_USER_NAME${NC}"
-    echo "Group Name: ${PURPLE}$USERSTORE_GROUP_NAME${NC}"
-    echo "Employee Number: ${PURPLE}000111${NC}"
-    echo "Cost Center: ${PURPLE}111111${NC}"
-    echo "Organization: ${PURPLE}WSO2Org${NC}"
-    echo "Division: ${PURPLE}$USERSTORE_GROUP_NAME${NC}"
-    echo "Department: ${PURPLE}Integration${NC}"
-    echo "Manager ID: ${PURPLE}111000${NC}"
-    echo "Manager Display Name: ${PURPLE}$USERSTORE_USER_NAME${NC}"
+    echo -e "User Name: ${PURPLE}$USERSTORE_USER_NAME${NC}"
+    echo -e "Group Name: ${PURPLE}$USERSTORE_GROUP_NAME${NC}"
+    echo -e "Employee Number: ${PURPLE}000111${NC}"
+    echo -e "Cost Center: ${PURPLE}111111${NC}"
+    echo -e "Organization: ${PURPLE}WSO2Org${NC}"
+    echo -e "Division: ${PURPLE}$USERSTORE_GROUP_NAME${NC}"
+    echo -e "Department: ${PURPLE}Integration${NC}"
+    echo -e "Manager ID: ${PURPLE}111000${NC}"
+    echo -e "Manager Display Name: ${PURPLE}$USERSTORE_USER_NAME${NC}"
 else
     # If there is an error, print the failure message with the error description
     error_description=$(echo "$response" | jq -r '.error[0].description')
