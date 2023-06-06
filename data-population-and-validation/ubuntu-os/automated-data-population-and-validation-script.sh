1d3faf9d473bb9f40a2c9f5e69ecef6089db3d32
JayanaGunaweera01 <jayana.20200003@iit.ac.lk>
2023-06-07 00:07:07 +0530

    Did some minor changes

--- data-population-and-validation/ubuntu-os/automated-data-population-and-validation-script.sh
+++ data-population-and-validation/ubuntu-os/automated-data-population-and-validation-script.sh
@@ -24,6 +24,8 @@ for script in \
   "1-user-creation/create-user.sh" \
   "1-user-creation/create-bulk-users.sh" \
   "2-tenant-creation/create-tenant.sh" \
+  "2-tenant-creation/register-an-app-in-a-tenant.sh" \
+  "2-tenant-creation/get-access-token-tenantwise.sh" \
   "3-userstore-creation/create-userstore.sh" \
   "3-userstore-creation/create-user-in-userstore.sh" \
   "4-service-provider-creation/register-a-service-provider.sh" \
