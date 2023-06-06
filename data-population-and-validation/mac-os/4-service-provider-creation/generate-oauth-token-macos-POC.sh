60933c12de2b37ed49ca6a1066587a01b1578e75
JayanaGunaweera01 <jayana.20200003@iit.ac.lk>
2023-06-06 14:59:09 +0530

    Did some minor changes

--- data-population-and-validation/mac-os/4-service-provider-creation/generate-oauth-token-macos-POC.sh
+++ data-population-and-validation/mac-os/4-service-provider-creation/generate-oauth-token-macos-POC.sh
@@ -13,35 +13,36 @@ script_dir="/Users/runner/work/Automating-Product-Migration-Testing/Automating-P
 
 # Load client_id and client_secret from file
 if [ -f "$script_dir/client_credentials" ]; then
-  echo "${YELLOW}${BOLD}Client Credentials File:${NC}"
+  echo -e "${YELLOW}${BOLD}Client Credentials File:${NC}"
   cat "$script_dir/client_credentials"
   . "/Users/runner/work/Automating-Product-Migration-Testing/Automating-Product-Migration-Testing/data-population-and-validation/mac-os/4-service-provider-creation/client_credentials"
-  echo "${GREEN}Client_credentials sourced.${NC}"
+  echo -e "${GREEN}Client_credentials sourced.${NC}"
 else
-  echo "${RED}${BOLD}Error: client_credentials file not found.${NC}"
+  echo -e "${RED}${BOLD}Error: client_credentials file not found.${NC}"
   exit 1
 fi
 
 base64_encoded=$(printf "%s:%s" "$client_id" "$client_secret" | base64)
 
 # Get access token
-echo "${PURPLE}Getting access token...${NC}"
-token_response=$(curl -ks -X POST https://localhost:9443/oauth2/token \
+echo -e "${PURPLE}Getting access token...${NC}"
+curl_response=$(curl -ks -X POST https://localhost:9443/oauth2/token \
   -H "Authorization: Basic $base64_encoded" \
   -H 'Content-Type: application/x-www-form-urlencoded' \
   -d 'grant_type=password&username=admin&password=admin&scope=somescope_password')
 
-echo "Token_response: $token_response"
+echo "Curl_response: $curl_response"
 # Extract access token and refresh token from response
-access_token=$(echo "$token_response" | jq -r '.access_token')
-refresh_token=$(echo "$token_response" | jq -r '.refresh_token')
+access_token=$(echo "$curl_response" | jq -r '.access_token')
+refresh_token=$(echo "$curl_response" | jq -r '.refresh_token')
 
 # Print access token and refresh token
 if [ "$access_token" != "null" ]; then
-  echo "Access token: ${PURPLE}$access_token${NC}"
+  echo -e "Access token: ${PURPLE}$access_token${NC}"
+  echo -e "Expires in: $(echo "$curl_response" | jq -r '.expires_in') seconds"
 fi
 if [ "$refresh_token" != "null" ]; then
-  echo "Refresh token: ${PURPLE}$refresh_token${NC}"
+  echo -e "Refresh token: ${PURPLE}$refresh_token${NC}"
 fi
 
 # Store access token and refresh token in a file
@@ -56,3 +57,4 @@ if grep -q "refresh_token" "$script_dir/client_credentials"; then
 else
   echo "refresh_token=$refresh_token" >>"$script_dir/client_credentials"
 fi
+
