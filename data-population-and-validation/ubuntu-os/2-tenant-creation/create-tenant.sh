b06add7b7802fae77bf066dc7bd4ba8c5cf93f4b
JayanaGunaweera01 <jayana.20200003@iit.ac.lk>
2023-06-06 23:32:37 +0530

    Did some minor changes

--- data-population-and-validation/ubuntu-os/2-tenant-creation/create-tenant.sh
+++ data-population-and-validation/ubuntu-os/2-tenant-creation/create-tenant.sh
@@ -28,6 +28,8 @@ response=$(curl -k --location --request POST "$TENANT_EP" \
   --header 'Authorization: Basic YWRtaW46YWRtaW4=' \
   --data-raw '{"domain":"wso2.com","owners":[{"username":"dummyuser","password":"dummypassword","email":"dummyuser@wso2.com","firstname":"Dummy","lastname":"User","provisioningMethod":"inline-password","additionalClaims":[{"claim":"http://wso2.org/claims/telephone","value":"+94 76 318 6705"}]}]}')
 
+echo $response
+
 # Check if the response contains any error message
 if echo "$response" | grep -q '"error":'; then
   # If there is an error, print the failure message with the error description
