ee0d70de2ced16c53d0d3b407317def396a58317
JayanaGunaweera01 <jayana.20200003@iit.ac.lk>
2023-06-11 04:54:40 +0530

    Did some minor changes

--- data-population-and-validation/5-group-creation/create-groups-with-users.sh
+++ data-population-and-validation/5-group-creation/create-groups-with-users.sh
@@ -23,25 +23,59 @@ if [ "$os" = "macos-latest" ]; then
 
 fi
 
-# Create interns group
-interns_response=$(curl -s -k -X POST -H "Content-Type: application/json" -H "Authorization: Basic YWRtaW46YWRtaW4=" -d '{"displayName":"interns","members":[{"display":"Randul","value":"Randul","ref":"User","operation":"add"},{"display":"Chithara","value":"Chithara","ref":"User","operation":"add"},{"display":"Rukshan","value":"Rukshan","ref":"User","operation":"add"}]}' https://localhost:9443/scim2/Groups)
+#!/bin/bash
 
-# Check if the interns group creation was successful
-if echo "$interns_response" | grep -q '"displayName":"interns"'; then
-  # Print success message
-  echo -e "${PURPLE}${BOLD}The interns group has been created successfully.${NC}"
-  echo -e "${PURPLE}Additional Details:${NC}"
-  echo -e "${PURPLE}- Display Name:${NC} interns"
-  echo -e "${PURPLE}- Members:${NC}"
-  echo -e "${PURPLE}  - Randul${NC}"
-  echo -e "${PURPLE}  - Chithara${NC}"
-  echo -e "${PURPLE}  - Rukshan${NC}"
+# Define colors
+RED='\033[0;31m'
+GREEN='\033[1;38;5;206m'
+BOLD='\033[1m'
+NC='\033[0m' # No Color
+
+# Function to add a user to the PRIMARY user store
+add_user() {
+  local username=$1
+  local password=$2
+  
+  response=$(curl -k -s -o /dev/null -w "%{http_code}" -X POST -H "Content-Type: application/json" -H "Authorization: Basic YWRtaW46YWRtaW4=" -d '{
+    "schemas": [],
+    "userName": "'"${username}"'",
+    "password": "'"${password}"'",
+    "name": {
+      "familyName": "",
+      "givenName": "'"${username}"'"
+    }
+  }' https://localhost:9443/api/server/v1/userstores/PRIMARY/users)
+
+  if [ "$response" -eq 201 ]; then
+    echo -e "${GREEN}${BOLD}User '${username}' has been added to the 'PRIMARY' user store successfully.${NC}"
+  else
+    echo -e "${RED}${BOLD}Failed to add user '${username}' to the 'PRIMARY' user store.${NC}"
+  fi
+}
+
+# Add users to the PRIMARY user store
+add_user "Jayana" "password123"
+add_user "Randul" "password123"
+add_user "Chithara" "password123"
+add_user "Rukshan" "password123"
+
+# Create the interns group
+response=$(curl -k -s -o /dev/null -w "%{http_code}" -X POST -H "Content-Type: application/json" -H "Authorization: Basic YWRtaW46YWRtaW4=" -d '{
+  "displayName": "interns",
+  "members": [
+    {"display": "Jayana", "value": "Jayana", "ref": "User", "operation": "add"},
+    {"display": "Randul", "value": "Randul", "ref": "User", "operation": "add"},
+    {"display": "Chithara", "value": "Chithara", "ref": "User", "operation": "add"},
+    {"display": "Rukshan", "value": "Rukshan", "ref": "User", "operation": "add"}
+  ]
+}' https://localhost:9443/api/identity/group/v1.0/groups)
+
+if [ "$response" -eq 201 ]; then
+  echo -e "${GREEN}${BOLD}Group 'interns' has been created successfully.${NC}"
 else
-  # Print failure message
-  echo -e "${RED}${BOLD}Failed to create the interns group.${NC}"
-  echo -e "${RED}${BOLD}Error Response: ${interns_response}${NC}"
+  echo -e "${RED}${BOLD}Failed to create the 'interns' group.${NC}"
 fi
-echo
+
 
 # Create interns group
 #curl -k -X POST -H "Content-Type: application/json" -H "Authorization: Basic YWRtaW46YWRtaW4=" -d '{"displayName":"interns","members":[{"display":"Jayana","value":"Jayana","ref":"User","operation":"add"},{"display":"Randul","value":"Randul","ref":"User","operation":"add"},{"display":"Chithara","value":"Chithara","ref":"User","operation":"add"},{"display":"Rukshan","value":"Rukshan","ref":"User","operation":"add"}]}' https://localhost:9443/api/identity/group/v1.0/groups
