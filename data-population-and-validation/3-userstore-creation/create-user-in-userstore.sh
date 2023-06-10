79a97c0c238df1f000ca7ffb2de74c89daa7a231
JayanaGunaweera01 <jayana.20200003@iit.ac.lk>
2023-06-11 02:08:04 +0530

    Did some minor changes

--- data-population-and-validation/3-userstore-creation/create-user-in-userstore.sh
+++ data-population-and-validation/3-userstore-creation/create-user-in-userstore.sh
@@ -26,6 +26,7 @@ fi
 response=$(curl -k --location --request POST "https://localhost:9443/api/server/v1/userstores/$USERSTORE_NAME/users" \
     --header 'Content-Type: application/json' \
     --header 'Authorization: Basic YWRtaW46YWRtaW4=' \
+    --header 'X-Requested-With: XMLHttpRequest' \
     --data-raw '{
         "schemas": [],
         "userName": "'"$USERSTORE_USER_NAME"'",
@@ -46,10 +47,11 @@ response=$(curl -k --location --request POST "https://localhost:9443/api/server/
 # Print the response
 echo -e "Response:\n$response"
 
-# Get the HTTP status code from the response
-http_status=$(echo "$response" | jq -r '.status')
-
-if [ "$http_status" == "201" ]; then
+# Check if the response contains an error
+error_description=$(echo "$response" | jq -r '.description')
+if [ "$error_description" != "null" ]; then
+    echo -e "${RED}${BOLD}Failure: $error_description${NC}"
+else
     echo -e "${GREEN}${BOLD}A user has been created in the userstore '$USERSTORE_NAME'. User name=${USERSTORE_USER_NAME}, Group name=${USERSTORE_GROUP_NAME}${NC}"
     echo -e "${PURPLE}Additional Details:${NC}"
     echo -e "User Name: ${PURPLE}$USERSTORE_USER_NAME${NC}"
@@ -61,7 +63,4 @@ if [ "$http_status" == "201" ]; then
     echo -e "Department: ${PURPLE}Integration${NC}"
     echo -e "Manager ID: ${PURPLE}111000${NC}"
     echo -e "Manager Display Name: ${PURPLE}$USERSTORE_USER_NAME${NC}"
-else
-    error_description=$(echo "$response" | jq -r '.error[0].description')
-    echo -e "${RED}${BOLD}Failure: $error_description${NC}"
 fi
