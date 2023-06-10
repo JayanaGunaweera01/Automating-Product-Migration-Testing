141fe71e09b6f53c7fa68c3f2962ec19d1157a17
JayanaGunaweera01 <jayana.20200003@iit.ac.lk>
2023-06-11 01:10:51 +0530

    Did some minor changes

--- data-population-and-validation/3-userstore-creation/create-user-in-userstore.sh
+++ data-population-and-validation/3-userstore-creation/create-user-in-userstore.sh
@@ -12,20 +12,17 @@ os=$1
 # Set deployment file and path based on OS
 if [ "$os" = "ubuntu-latest" ]; then
 
-  chmod +x env.sh
-  . "/home/runner/work/Automating-Product-Migration-Testing/Automating-Product-Migration-Testing/migration-automation/env.sh"
-  echo "${GREEN}==> Env file for Ubuntu sourced successfully"
+    chmod +x env.sh
+    . "/home/runner/work/Automating-Product-Migration-Testing/Automating-Product-Migration-Testing/migration-automation/env.sh"
+    echo "${GREEN}==> Env file for Ubuntu sourced successfully"
 fi
 if [ "$os" = "macos-latest" ]; then
 
-  chmod +x env.sh
-  source "/Users/runner/work/Automating-Product-Migration-Testing/Automating-Product-Migration-Testing/migration-automation/env.sh"
-  echo "${GREEN}==> Env file for Mac sourced successfully${RESET}"
+    chmod +x env.sh
+    source "/Users/runner/work/Automating-Product-Migration-Testing/Automating-Product-Migration-Testing/migration-automation/env.sh"
+    echo "${GREEN}==> Env file for Mac sourced successfully${RESET}"
 
 fi
-
-# Set the userstore name
-USERSTORE_NAME="$USERSTORE_NAME"
 
 # Create the user in the userstore
 response=$(curl -k --location --request POST "https://localhost:9443/api/server/v1/userstores/$USERSTORE_NAME/users" \
@@ -51,12 +48,12 @@ response=$(curl -k --location --request POST "https://localhost:9443/api/server/
 # Print the response
 echo -e "Response:\n$response"
 
-# Check if the response contains any error message
-if echo "$response" | grep -q '"schemas":'; then
+# Check if the response has a successful HTTP status code
+if [ "$(echo "$response" | jq -r '.status')" = "success" ]; then
     # If there is no error, print the success message
     echo -e "${GREEN}${BOLD}A user has been created in the userstore '$USERSTORE_NAME'. User name=${USERSTORE_USER_NAME}, Group name=${USERSTORE_GROUP_NAME}${NC}"
     # Print the additional details
-    echo "{PURPLE}Additional Details:${NC}"
+    echo -e "${PURPLE}Additional Details:${NC}"
     echo -e "User Name: ${PURPLE}$USERSTORE_USER_NAME${NC}"
     echo -e "Group Name: ${PURPLE}$USERSTORE_GROUP_NAME${NC}"
     echo -e "Employee Number: ${PURPLE}000111${NC}"
@@ -71,4 +68,3 @@ else
     error_description=$(echo "$response" | jq -r '.error[0].description')
     echo -e "${RED}${BOLD}Failure: $error_description${NC}"
 fi
-echo
