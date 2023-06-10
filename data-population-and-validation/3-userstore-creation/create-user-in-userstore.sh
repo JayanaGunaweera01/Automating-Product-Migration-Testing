2498adf0bf8409b3802f7418bcdee6144c31d8c7
JayanaGunaweera01 <jayana.20200003@iit.ac.lk>
2023-06-11 01:57:23 +0530

    Did some minor changes

--- data-population-and-validation/3-userstore-creation/create-user-in-userstore.sh
+++ data-population-and-validation/3-userstore-creation/create-user-in-userstore.sh
@@ -11,17 +11,15 @@ os=$1
 
 # Set deployment file and path based on OS
 if [ "$os" = "ubuntu-latest" ]; then
-
     chmod +x env.sh
     . "/home/runner/work/Automating-Product-Migration-Testing/Automating-Product-Migration-Testing/migration-automation/env.sh"
     echo "${GREEN}==> Env file for Ubuntu sourced successfully"
 fi
-if [ "$os" = "macos-latest" ]; then
 
+if [ "$os" = "macos-latest" ]; then
     chmod +x env.sh
     source "/Users/runner/work/Automating-Product-Migration-Testing/Automating-Product-Migration-Testing/migration-automation/env.sh"
     echo "${GREEN}==> Env file for Mac sourced successfully${RESET}"
-
 fi
 
 # Create the user in the userstore
@@ -47,12 +45,13 @@ response=$(curl -k --location --request POST "https://localhost:9443/api/server/
 
 # Print the response
 echo -e "Response:\n$response"
-# Get the HTTP status code
-http_status=$(curl -s -o /dev/null -w "%{http_code}" "https://localhost:9443/api/server/v1/userstores/$USERSTORE_NAME/users")
 
-if [ $http_status -eq 201 ]; then
+# Get the HTTP status code from the response
+http_status=$(echo "$response" | jq -r '.status')
+
+if [ "$http_status" == "201" ]; then
     echo -e "${GREEN}${BOLD}A user has been created in the userstore '$USERSTORE_NAME'. User name=${USERSTORE_USER_NAME}, Group name=${USERSTORE_GROUP_NAME}${NC}"
-    echo "{PURPLE}Additional Details:${NC}"
+    echo -e "${PURPLE}Additional Details:${NC}"
     echo -e "User Name: ${PURPLE}$USERSTORE_USER_NAME${NC}"
     echo -e "Group Name: ${PURPLE}$USERSTORE_GROUP_NAME${NC}"
     echo -e "Employee Number: ${PURPLE}000111${NC}"
@@ -63,5 +62,6 @@ if [ $http_status -eq 201 ]; then
     echo -e "Manager ID: ${PURPLE}111000${NC}"
     echo -e "Manager Display Name: ${PURPLE}$USERSTORE_USER_NAME${NC}"
 else
-    echo -e "${RED}${BOLD}Failure: User creation request returned HTTP status code $http_status${NC}"
+    error_description=$(echo "$response" | jq -r '.error[0].description')
+    echo -e "${RED}${BOLD}Failure: $error_description${NC}"
 fi
