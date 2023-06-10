d808a7c09cc2d16bbc1b8de0f2a893e5d375bff3
JayanaGunaweera01 <jayana.20200003@iit.ac.lk>
2023-06-11 05:04:01 +0530

    Did some minor changes

--- data-population-and-validation/5-group-creation/create-groups-with-users.sh
+++ data-population-and-validation/5-group-creation/create-groups-with-users.sh
@@ -6,36 +6,11 @@ GREEN='\033[1;38;5;206m'
 BOLD='\033[1m'
 NC='\033[0m' # No Color
 
-os=$1
-
-# Set deployment file and path based on OS
-if [ "$os" = "ubuntu-latest" ]; then
-
-  chmod +x env.sh
-  . "/home/runner/work/Automating-Product-Migration-Testing/Automating-Product-Migration-Testing/migration-automation/env.sh"
-  echo "${GREEN}==> Env file for Ubuntu sourced successfully"
-fi
-if [ "$os" = "macos-latest" ]; then
-
-  chmod +x env.sh
-  source "/Users/runner/work/Automating-Product-Migration-Testing/Automating-Product-Migration-Testing/migration-automation/env.sh"
-  echo "${GREEN}==> Env file for Mac sourced successfully${RESET}"
-
-fi
-
-#!/bin/bash
-
-# Define colors
-RED='\033[0;31m'
-GREEN='\033[1;38;5;206m'
-BOLD='\033[1m'
-NC='\033[0m' # No Color
-
 # Function to add a user to the PRIMARY user store
 add_user() {
   local username=$1
   local password=$2
-  
+
   response=$(curl -k -s -o /dev/null -w "%{http_code}" -X POST -H "Content-Type: application/json" -H "Authorization: Basic YWRtaW46YWRtaW4=" -d '{
     "schemas": [],
     "userName": "'"${username}"'",
@@ -50,6 +25,7 @@ add_user() {
     echo -e "${GREEN}${BOLD}User '${username}' has been added to the 'PRIMARY' user store successfully.${NC}"
   else
     echo -e "${RED}${BOLD}Failed to add user '${username}' to the 'PRIMARY' user store.${NC}"
+    echo "Response Code: $response"
   fi
 }
 
@@ -74,6 +50,7 @@ if [ "$response" -eq 201 ]; then
   echo -e "${GREEN}${BOLD}Group 'interns' has been created successfully.${NC}"
 else
   echo -e "${RED}${BOLD}Failed to create the 'interns' group.${NC}"
+  echo "Response Code: $response"
 fi
 
 
