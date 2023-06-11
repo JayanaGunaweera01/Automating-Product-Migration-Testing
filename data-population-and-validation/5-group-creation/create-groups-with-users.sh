863c3e0170978becbb14f8813ad5e716db2a295d
JayanaGunaweera01 <jayana.20200003@iit.ac.lk>
2023-06-11 06:07:14 +0530

    Did some minor changes

--- data-population-and-validation/5-group-creation/create-groups-with-users.sh
+++ data-population-and-validation/5-group-creation/create-groups-with-users.sh
@@ -3,56 +3,54 @@
 # Define colors
 RED='\033[0;31m'
 GREEN='\033[1;38;5;206m'
-PURPLE='\033[1;35m'
 BOLD='\033[1m'
 NC='\033[0m' # No Color
 
-# Identity Server URL and credentials
-IS_URL="https://localhost:9443"
-USERNAME="admin"
-PASSWORD="admin"
+# Function to add a user to the PRIMARY user store
+add_user() {
+  local username=$1
+  local password=$2
 
-# User details
-USERS=("Jayana" "Randul" "Chithara" "Rukshan")
+  response=$(curl -k -s -o /dev/null -w "%{http_code}" -X POST -H "Content-Type: application/json" -H "Authorization: Basic YWRtaW46YWRtaW4=" -d '{
+    "schemas": [],
+    "userName": "'"${username}"'",
+    "password": "'"${password}"'",
+    "name": {
+      "familyName": "",
+      "givenName": "'"${username}"'"
+    }
+  }' https://localhost:9443/api/server/v1/userstores/PRIMARY/users)
 
-# Create users in the PRIMARY user store
-for user in "${USERS[@]}"; do
-    response=$(curl -k --location --request POST "$IS_URL/api/identity/user/v1.0/me/user-stores/PRIMARY/users" \
-        --header 'Content-Type: application/json' \
-        --header "Authorization: Basic $(echo -n "$USERNAME:$PASSWORD" | base64)" \
-        --data-raw '{
-            "schemas": ["urn:ietf:params:scim:schemas:core:2.0:User"],
-            "userName": "'"$user"'",
-            "password": "Userstorepassword@123"
-        }')
+  if [ "$response" -eq 201 ]; then
+    echo -e "${GREEN}${BOLD}User '${username}' has been added to the 'PRIMARY' user store successfully.${NC}"
+  else
+    echo -e "${RED}${BOLD}Failed to add user '${username}' to the 'PRIMARY' user store.${NC}"
+    echo "Response Code: $response"
+  fi
+}
 
-    # Check if the user creation is successful
-    if [[ $(echo "$response" | jq -r '.userName') == "$user" ]]; then
-        echo -e "User '${GREEN}${BOLD}$user${NC}' created successfully"
-    else
-        echo -e "${RED}${BOLD}Failed to add user '$user' to the 'PRIMARY' user store.${NC}"
-        echo "Response Code: $(echo "$response" | jq -r '.status')"
-    fi
-done
+# Add users to the PRIMARY user store
+add_user "Jayana" "jayanapass"
+add_user "Randul" "randulpass"
+add_user "Chithara" "chitharapass"
+add_user "Rukshan" "rukshanpass"
 
-# Create the 'interns' group
-group_response=$(curl -k -X POST -H "Content-Type: application/json" -H "Authorization: Basic $(echo -n "$USERNAME:$PASSWORD" | base64)" \
-    -d '{
-        "displayName": "interns",
-        "members": [
-            {"display": "Jayana", "value": "Jayana", "ref": "User", "operation": "add"},
-            {"display": "Randul", "value": "Randul", "ref": "User", "operation": "add"},
-            {"display": "Chithara", "value": "Chithara", "ref": "User", "operation": "add"},
-            {"display": "Rukshan", "value": "Rukshan", "ref": "User", "operation": "add"}
-        ]
-    }' "$IS_URL/api/identity/group/v1.0/groups")
+# Create the interns group
+response=$(curl -k -s -o /dev/null -w "%{http_code}" -X POST -H "Content-Type: application/json" -H "Authorization: Basic YWRtaW46YWRtaW4=" -d '{
+  "displayName": "interns",
+  "members": [
+    {"display": "Jayana", "value": "Jayana", "ref": "User", "operation": "add"},
+    {"display": "Randul", "value": "Randul", "ref": "User", "operation": "add"},
+    {"display": "Chithara", "value": "Chithara", "ref": "User", "operation": "add"},
+    {"display": "Rukshan", "value": "Rukshan", "ref": "User", "operation": "add"}
+  ]
+}' https://localhost:9443/api/identity/group/v1.0/groups)
 
-# Check if the group creation is successful
-if [[ $(echo "$group_response" | jq -r '.displayName') == "interns" ]]; then
-    echo -e "Group '${GREEN}${BOLD}interns${NC}' created successfully"
+if [ "$response" -eq 201 ]; then
+  echo -e "${GREEN}${BOLD}Group 'interns' has been created successfully.${NC}"
 else
-    echo -e "${RED}${BOLD}Failed to create the 'interns' group.${NC}"
-    echo "Response Code: $(echo "$group_response" | jq -r '.status')"
+  echo -e "${RED}${BOLD}Failed to create the 'interns' group.${NC}"
+  echo "Response Code: $response"
 fi
 
 
