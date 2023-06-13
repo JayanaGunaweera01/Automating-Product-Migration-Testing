065368fb6e47f8bbdc3fdb3a1ea813ca6014b3f0
JayanaGunaweera01 <jayana.20200003@iit.ac.lk>
2023-06-13 14:30:25 +0530

    Did some minor changes

--- data-population-and-validation/3-userstore-creation/create-user-in-userstore.sh
+++ data-population-and-validation/3-userstore-creation/create-user-in-userstore.sh
@@ -56,6 +56,13 @@ user_store_response=$(curl -k --location --request POST "https://localhost:9443/
 
 echo "Userstore Creation Response: "$user_store_response""
 
+user_creation_response=$(curl -k --location --request POST "$SCIM_USER_EP_USERSTORE" \
+--header 'Content-Type: application/json' \
+--header 'Authorization: Basic YWRtaW46YWRtaW4=' \
+--data-raw '{"schemas":[],"userName":"'$USERSTORE_USER_NAME'","password":"'$USERSTORE_USER_PASSWORD'","wso2Extension":{"employeeNumber":"000111","costCenter":"111111","organization":"WSO2Org","division":"'$USERSTORE_GROUP_NAME'","department":"Intigration","manager":{"managerId":"111000","displayName":"'$USERSTORE_USER_NAME'"}}}')
+
+echo "User Creation Response: "$user_creation_response""
+
 if [ -n "$user_store_response" ]; then
     echo -e "${PURPLE}${BOLD}User store 'Testuserstore' has been created successfully.${NC}"
     echo -e "${PURPLE}${BOLD}User Store Response:${NC}"
