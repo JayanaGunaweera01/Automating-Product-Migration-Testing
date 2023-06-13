aa28eae7252f48889cf37b945d17bebfe35d1bd5
JayanaGunaweera01 <jayana.20200003@iit.ac.lk>
2023-06-14 01:57:08 +0530

    Did some minor changes

--- data-population-and-validation/3-userstore-creation/create-user-in-userstore.sh
+++ data-population-and-validation/3-userstore-creation/create-user-in-userstore.sh
@@ -57,91 +57,92 @@ user_store_response=$(curl -k --location --request POST "https://localhost:9443/
 
 echo "Userstore Creation Response: "$user_store_response""
 
-
 if [ -n "$user_store_response" ]; then
     echo -e "${PURPLE}${BOLD}User store 'Testuserstore' has been created successfully.${NC}"
     echo -e "${PURPLE}${BOLD}User Store Response:${NC}"
     echo "$user_store_response"
-
-cd /home/runner/work/Automating-Product-Migration-Testing/Automating-Product-Migration-Testing/migration-automation/IS_HOME_OLD/wso2is-5.11.0/repository/deployment/server/userstores
-
-# Find the file containing the specified property
-file=Testuserstore.xml
-
-if [ -n "$file" ]; then
-  echo "File found: $file"
-  
-  # Check if the property already exists in the file
-  if ! grep -q '<Property name="SCIMEnabled">true</Property>' "$file"; then
-    # Add the property to the file right below the specified line with proper indentation
-    sed -i '/<Property name="Description">Sample JDBC user store to add.<\/Property>/a \ \ <Property name="SCIMEnabled">true<\/Property>' "$file"
-    echo "Property added successfully."
-  else
-    echo "Property already exists in the file."
-  fi
-  
-  # Display the contents of the file
-  cat "$file"
-else
-  echo "File not found."
 fi
 
-chmod +x Testuserstore.xml
+# Create a user in the userstore
+user_creation_response=$(curl -k --location --request POST "$SCIM_USER_EP_USERSTORE" \
+    --header 'Content-Type: application/json' \
+    --header 'Authorization: Basic YWRtaW46YWRtaW4=' \
+    --data-raw '{"schemas":[],"userName":"'$USERSTORE_USER_NAME'","password":"'$USERSTORE_USER_PASSWORD'","wso2Extension":{"employeeNumber":"000111","costCenter":"111111","organization":"WSO2Org","division":"'$USERSTORE_GROUP_NAME'","department":"Intigration","manager":{"managerId":"111000","displayName":"'$USERSTORE_USER_NAME'"}}}')
+echo -e "${PURPLE}${BOLD}A user has been created in the userstore successfully.${NC}"
+echo -e "${PURPLE}${BOLD}User Creation Response${NC}: "$user_creation_response""
 
- # Create a user in the userstore
-    user_creation_response=$(curl -k --location --request POST "$SCIM_USER_EP_USERSTORE" \
-        --header 'Content-Type: application/json' \
-        --header 'Authorization: Basic YWRtaW46YWRtaW4=' \
-        --data-raw '{"schemas":[],"userName":"'$USERSTORE_USER_NAME'","password":"'$USERSTORE_USER_PASSWORD'","wso2Extension":{"employeeNumber":"000111","costCenter":"111111","organization":"WSO2Org","division":"'$USERSTORE_GROUP_NAME'","department":"Intigration","manager":{"managerId":"111000","displayName":"'$USERSTORE_USER_NAME'"}}}')
-    echo -e "${PURPLE}${BOLD}A user has been created in the userstore successfully.${NC}"
-    echo -e "${PURPLE}${BOLD}User Creation Response${NC}: "$user_creation_response""
 
- #   response=$(curl -v -k --user admin:admin --data '{"schemas":[],"name":{"familyName":"jackson","givenName":"kim"},"userName":"Testuserstore/kim","password":"kimwso2","emails":[{"primary":true,"value":"kim.jackson@gmail.com","type":"home"},{"value":"kim_j@wso2.com","type":"work"}]}' --header "Content-Type:application/json" https://localhost:9443/scim2/Users)
- #   echo "$response"
+#cd /home/runner/work/Automating-Product-Migration-Testing/Automating-Product-Migration-Testing/migration-automation/IS_HOME_OLD/wso2is-5.11.0/repository/deployment/server/userstores
 
- #   response=$(curl -k --user admin:admin --data '{"displayName": "Testuserstore/AMRSNGHE"}' --header "Content-Type: application/json" https://localhost:9443/wso2/scim/Groups)
- #   echo "$response"
-    # Create a group in the user store domain
- #   group_response=$(curl -k --user admin:admin --data '{"displayName": "Testuserstore/AMRSNGHE"}' --header "Content-Type: application/json" https://localhost:9443/wso2/scim/Groups)
- #   echo "Group Creation Response: "$group_response""
- #   group_id=$(echo "$group_response" | jq -r '.id')
+# Find the file containing the specified property
+#file=Testuserstore.xml
 
- #   if [ -n "$group_id" ]; then
- #       echo -e "${PURPLE}${BOLD}Group has been created successfully in the user store domain.${NC}"
- #       echo -e "${PURPLE}${BOLD}Group Response:${NC}"
- #       echo "$group_response"
+#if [ -n "$file" ]; then
+#  echo "File found: $file"
 
-        # Create a user in the given user store domain
- #       user_response=$(curl -k --user admin:admin --data '{"schemas":[],"name":{"familyName":"John","givenName":"Doe"},"userName":"Testuserstore/AMRSNGHE","password":"testPwd123"}' --header "Content-Type: application/json" https://localhost:9443/wso2/scim/Users)
-  #      echo "User Creation Response: "$user_response""
-  #      user_id=$(echo "$user_response" | jq -r '.id')
+# Check if the property already exists in the file
+# if ! grep -q '<Property name="SCIMEnabled">true</Property>' "$file"; then
+# Add the property to the file right below the specified line with proper indentation
+#   sed -i '/<Property name="Description">Sample JDBC user store to add.<\/Property>/a \ \ <Property name="SCIMEnabled">true<\/Property>' "$file"
+#   echo "Property added successfully."
+# else
+#   echo "Property already exists in the file."
+# fi
 
- #       if [ -n "$user_id" ]; then
-  #          echo -e "${PURPLE}${BOLD}User has been created successfully in the given user store domain.${NC}"
-  #          echo -e "${PURPLE}${BOLD}User Response:${NC}"
-   #         echo "$user_response"
+# Display the contents of the file
+# cat "$file"
+#else
+# echo "File not found."
+#fi
 
-            # Add the user to the created group
-  #          add_user_to_group_response=$(curl -k --user admin:admin --request PATCH --data '{"members":[{"value":"'$user_id'"}]}' --header "Content-Type: application/json" "https://localhost:9443/wso2/scim/Groups/$group_id")
-  #          echo "User Adding To The Group Response: "$add_user_to_group_response""
-  #          if [ -n "$add_user_to_group_response" ]; then
-  #              echo -e "${PURPLE}${BOLD}User has been added to the group successfully.${NC}"
-  #              echo -e "${PURPLE}${BOLD}Add User to Group Response:${NC}"
-   #             echo "$add_user_to_group_response"
-   #         else
-   #             echo -e "${RED}${BOLD}Failed to add the user to the group.${NC}"
-   #             echo -e "${RED}${BOLD}Error Message:${NC} $add_user_to_group_response"
-   #         fi
-  #      else
-  #          echo -e "${RED}${BOLD}Failed to create the user in the given user store domain.${NC}"
-  #          echo -e "${RED}${BOLD}Error Message:${NC} $user_response"
-  #      fi
-  #  else
- #       echo -e "${RED}${BOLD}Failed to create the group in the user store domain.${NC}"
- #       echo -e "${RED}${BOLD}Error Message:${NC} $group_response"
-  #  fi
+#chmod +x Testuserstore.xml
+
+#   response=$(curl -v -k --user admin:admin --data '{"schemas":[],"name":{"familyName":"jackson","givenName":"kim"},"userName":"Testuserstore/kim","password":"kimwso2","emails":[{"primary":true,"value":"kim.jackson@gmail.com","type":"home"},{"value":"kim_j@wso2.com","type":"work"}]}' --header "Content-Type:application/json" https://localhost:9443/scim2/Users)
+#   echo "$response"
+
+#   response=$(curl -k --user admin:admin --data '{"displayName": "Testuserstore/AMRSNGHE"}' --header "Content-Type: application/json" https://localhost:9443/wso2/scim/Groups)
+#   echo "$response"
+# Create a group in the user store domain
+#   group_response=$(curl -k --user admin:admin --data '{"displayName": "Testuserstore/AMRSNGHE"}' --header "Content-Type: application/json" https://localhost:9443/wso2/scim/Groups)
+#   echo "Group Creation Response: "$group_response""
+#   group_id=$(echo "$group_response" | jq -r '.id')
+
+#   if [ -n "$group_id" ]; then
+#       echo -e "${PURPLE}${BOLD}Group has been created successfully in the user store domain.${NC}"
+#       echo -e "${PURPLE}${BOLD}Group Response:${NC}"
+#       echo "$group_response"
+
+# Create a user in the given user store domain
+#       user_response=$(curl -k --user admin:admin --data '{"schemas":[],"name":{"familyName":"John","givenName":"Doe"},"userName":"Testuserstore/AMRSNGHE","password":"testPwd123"}' --header "Content-Type: application/json" https://localhost:9443/wso2/scim/Users)
+#      echo "User Creation Response: "$user_response""
+#      user_id=$(echo "$user_response" | jq -r '.id')
+
+#       if [ -n "$user_id" ]; then
+#          echo -e "${PURPLE}${BOLD}User has been created successfully in the given user store domain.${NC}"
+#          echo -e "${PURPLE}${BOLD}User Response:${NC}"
+#         echo "$user_response"
+
+# Add the user to the created group
+#          add_user_to_group_response=$(curl -k --user admin:admin --request PATCH --data '{"members":[{"value":"'$user_id'"}]}' --header "Content-Type: application/json" "https://localhost:9443/wso2/scim/Groups/$group_id")
+#          echo "User Adding To The Group Response: "$add_user_to_group_response""
+#          if [ -n "$add_user_to_group_response" ]; then
+#              echo -e "${PURPLE}${BOLD}User has been added to the group successfully.${NC}"
+#              echo -e "${PURPLE}${BOLD}Add User to Group Response:${NC}"
+#             echo "$add_user_to_group_response"
+#         else
+#             echo -e "${RED}${BOLD}Failed to add the user to the group.${NC}"
+#             echo -e "${RED}${BOLD}Error Message:${NC} $add_user_to_group_response"
+#         fi
+#      else
+#          echo -e "${RED}${BOLD}Failed to create the user in the given user store domain.${NC}"
+#          echo -e "${RED}${BOLD}Error Message:${NC} $user_response"
+#      fi
+#  else
+#       echo -e "${RED}${BOLD}Failed to create the group in the user store domain.${NC}"
+#       echo -e "${RED}${BOLD}Error Message:${NC} $group_response"
+#  fi
 
 #else
- #   echo -e "${RED}${BOLD}Failed to create the user store.${NC}"
- #   echo -e "${RED}${BOLD}Error Message:${NC} $user_store_response"
+#   echo -e "${RED}${BOLD}Failed to create the user store.${NC}"
+#   echo -e "${RED}${BOLD}Error Message:${NC} $user_store_response"
 #fi
