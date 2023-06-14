3ee96d0762a6d485135751759a025c0a5d8065a6
JayanaGunaweera01 <jayana.20200003@iit.ac.lk>
2023-06-14 19:01:20 +0530

    Did some minor changes

--- migration-automation/ubuntu-os/migration-script-ubuntu.sh
+++ migration-automation/ubuntu-os/migration-script-ubuntu.sh
@@ -94,10 +94,58 @@ unzip -qq *.zip &
 wait
 echo "${GREEN}==> Unzipped downloaded Identity Server zip${RESET}"
 
-# Run update tool
-cd "$UBUNTU_HOME"
-chmod +x update-pack.sh
-sh update-pack.sh "$email" "$password" "$currentVersion"
+# Copy update tool from utils to bin folder
+cd "/home/runner/work/Automating-Product-Migration-Testing/Automating-Product-Migration-Testing/utils/update-tools"
+
+cp -r $UPDATE_TOOL_UBUNTU $BIN_ISOLD
+copy_exit_code=$?
+if [ $copy_exit_code -eq 0 ]; then
+    echo "${GREEN}==> Update tool successfully copied to $currentVersion${RESET}"
+else
+    echo "${RED}==> Failed to copy the update tool.${RESET}"
+fi
+
+cd "$BIN_ISOLD"
+
+sudo apt-get install expect -y
+
+# Create an expect script file
+cat >wso2update_script.expect <<EOF
+#!/usr/bin/expect -f
+spawn ./wso2update_linux
+expect "Please enter your credentials to continue."
+sleep 5
+send -- "$email\r"
+expect "Email:"
+sleep 5
+send -- "$password\r"
+expect {
+    "wso2update: Error while authenticating user: Error while authenticating user credentials: Invalid email address '*'" {
+        puts "Invalid email address. Please check the MIGRATION_EMAIL environment variable."
+        exit 1
+    }
+    "wso2update: Error while authenticating user: Error while authenticating user credentials: Unable to read input: EOF" {
+        puts "Error while authenticating user credentials. Please check the MIGRATION_PASSWORD environment variable."
+        exit 1
+    }
+    eof {
+        puts "Updated the Client Tool successfully"
+        exit 0
+    }
+}
+EOF
+# Set executable permissions for the expect script
+chmod +x wso2update_script.expect
+# Run the expect script
+./wso2update_script.expect
+
+echo "${GREEN}==> Updated the Client Tool successfully${RESET}" &
+wait $!
+
+# Update Product Pack
+./wso2update_linux
+echo "${GREEN}==> Updated the Product Pack successfully${RESET}" &
+wait $!
 
 cd "$AUTOMATION_HOME"
 
@@ -183,10 +231,56 @@ unzip -qq *.zip &
 wait $!
 echo "${GREEN}==> Unzipped "$migratingVersion" zip${RESET}"
 
-# Run update tool
-cd "$UBUNTU_HOME"
-chmod +x update-pack.sh
-sh update-pack.sh "$email" "$password" "$migratingVersion"
+# Update package
+cp -r $UPDATE_TOOL_UBUNTU $BIN_ISNEW
+copy_exit_code=$?
+if [ $copy_exit_code -eq 0 ]; then
+    echo "${GREEN}==> Update tool successfully copied to $currentVersion${RESET}"
+else
+    echo "${RED}==> Failed to copy the update tool.${RESET}"
+fi
+
+cd "$BIN_ISNEW"
+
+sudo apt-get install expect -y
+
+# Create an expect script file
+cat >wso2update_script.expect <<EOF
+#!/usr/bin/expect -f
+spawn ./wso2update_linux
+expect "Please enter your credentials to continue."
+sleep 5
+send -- "$email\r"
+expect "Email:"
+sleep 5
+send -- "$password\r"
+expect {
+    "wso2update: Error while authenticating user: Error while authenticating user credentials: Invalid email address '*'" {
+        puts "Invalid email address. Please check the MIGRATION_EMAIL environment variable."
+        exit 1
+    }
+    "wso2update: Error while authenticating user: Error while authenticating user credentials: Unable to read input: EOF" {
+        puts "Error while authenticating user credentials. Please check the MIGRATION_PASSWORD environment variable."
+        exit 1
+    }
+    eof {
+        puts "Updated the Client Tool successfully"
+        exit 0
+    }
+}
+EOF
+# Set executable permissions for the expect script
+chmod +x wso2update_script.expect
+# Run the expect script
+./wso2update_script.expect
+
+echo "${GREEN}==> Updated the Client Tool successfully${RESET}" &
+wait $!
+
+# Update Product Pack
+./wso2update_linux
+echo "${GREEN}==> Updated the Product Pack successfully${RESET}" &
+wait $!
 
 # Divert to utils folder
 cd "$UTILS"
