f04b5fe3b88dd7eda82fef7608488b61f98f7082
Jayana  Gunaweera <79576139+JayanaGunaweera01@users.noreply.github.com>
2023-06-21 06:53:32 +0530

    Update migration-script-ubuntu.sh

--- migration-automation/ubuntu-os/migration-script-ubuntu.sh
+++ migration-automation/ubuntu-os/migration-script-ubuntu.sh
@@ -112,59 +112,60 @@ if [ "$currentVersion" = "5.9.0" ]; then
     sh update-pack.sh "$email" "$password" "current"
     wait $!
 else
-    # Copy update tool from utils to bin folder
-    cd "/home/runner/work/Automating-Product-Migration-Testing/Automating-Product-Migration-Testing/utils/update-tools"
-    chmod +x "$UPDATE_TOOL_UBUNTU"
-    cp -r "$UPDATE_TOOL_UBUNTU" "$BIN_ISOLD"
-    copy_exit_code=$?
-    if [ $copy_exit_code -eq 0 ]; then
-        echo "==> Update tool successfully copied to $currentVersion"
-    else
-        echo "==> Failed to copy the update tool."
-    fi
-
-    cd "$BIN_ISOLD"
-
-    sudo apt-get install expect -y
-
-    # Create an expect script file
-    cat >wso2update_script.expect <<EOF
-#!/usr/bin/expect -f
-spawn ./wso2update_linux
-expect "Please enter your credentials to continue."
-sleep 5
-send -- "$email\r"
-expect "Email:"
-sleep 5
-send -- "$password\r"
-expect {
-    "wso2update: Error while authenticating user: Error while authenticating user credentials: Invalid email address '*'" {
-        puts "Invalid email address. Please check the MIGRATION_EMAIL environment variable."
-        exit 1
-    }
-    "wso2update: Error while authenticating user: Error while authenticating user credentials: Unable to read input: EOF" {
-        puts "Error while authenticating user credentials. Please check the MIGRATION_PASSWORD environment variable."
-        exit 1
-    }
-    eof {
-        puts "Updated the Client Tool successfully"
-        exit 0
-    }
-}
-EOF
-
-    # Set executable permissions for the expect script
-    chmod +x wso2update_script.expect
-    # Run the expect script
-    ./wso2update_script.expect
-
-    echo "==> Updated the Client Tool successfully" &
-    wait $!
-
-    # Update Product Pack
-    ./wso2update_linux
-    echo "==> Updated the Product Pack successfully" &
-    wait $!
+     
+        # Copy update tool from utils to bin folder
+        cd "/home/runner/work/Automating-Product-Migration-Testing/Automating-Product-Migration-Testing/utils/update-tools"
+        
+        # Update package
+        cp -r $UPDATE_TOOL_UBUNTU $BIN_ISNEW
+        copy_exit_code=$?
+        if [ $copy_exit_code -eq 0 ]; then
+            echo "${GREEN}==> Update tool successfully copied to $currentVersion${RESET}"
+        else
+            echo "${RED}==> Failed to copy the update tool.${RESET}"
+        fi
+        
+        cd "$BIN_ISNEW"
+        
+        sudo apt-get install expect -y
+        
+        # Create an expect script file
+        cat >wso2update_script.expect <<EOF
+        #!/usr/bin/expect -f
+        spawn ./wso2update_linux
+        expect "Please enter your credentials to continue."
+        sleep 5
+        send -- "$email\r"
+        expect "Email:"
+        sleep 5
+        send -- "$password\r"
+        expect {
+            "wso2update: Error while authenticating user: Error while authenticating user credentials: Invalid email address '*'" {
+                puts "Invalid email address. Please check the MIGRATION_EMAIL environment variable."
+                exit 1
+            }
+            "wso2update: Error while authenticating user: Error while authenticating user credentials: Unable to read input: EOF" {
+                puts "Error while authenticating user credentials. Please check the MIGRATION_PASSWORD environment variable."
+                exit 1
+            }
+            eof {
+                puts "Updated the Client Tool successfully"
+                exit 0
+            }
+        }
+        EOF
+        # Set executable permissions for the expect script
+        chmod +x wso2update_script.expect
+        # Run the expect script
+        ./wso2update_script.expect
+        
+        echo "${GREEN}==> Updated the Client Tool successfully${RESET}" &
+        wait $!
+        
+        # Update Product Pack
+        ./wso2update_linux
+        echo "${GREEN}==> Updated the Product Pack successfully${RESET}" &
+        wait $!
 fi
 
 cd "$AUTOMATION_HOME"
