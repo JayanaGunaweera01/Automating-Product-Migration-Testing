3834f30af9564958e9fd5042ccfd35194688edda
Jayana  Gunaweera <79576139+JayanaGunaweera01@users.noreply.github.com>
2023-06-22 09:56:37 +0530

    Update create-bulk-users.sh

--- data-population-and-validation/1-user-creation/create-bulk-users.sh
+++ data-population-and-validation/1-user-creation/create-bulk-users.sh
@@ -25,13 +25,9 @@ if [ "$os" = "macos-latest" ]; then
 
 fi
 
-SCIM_BULK_EP="https://example.com/scim/bulk/endpoint"
-USERNAME="admin"
-PASSWORD="adminpass"
-
 response=$(curl -k --location --request POST "$SCIM_BULK_EP" \
     --header 'Content-Type: application/scim+json' \
-    --header "Authorization: Basic $(echo -n "$USERNAME:$PASSWORD" | base64)" \
+    --header 'Authorization: Basic YWRtaW46YWRtaW4=' \
     --data-raw '{
   "failOnErrors": 1,
   "schemas": ["urn:ietf:params:scim:api:messages:2.0:BulkRequest"],
@@ -110,7 +106,6 @@ response=$(curl -k --location --request POST "$SCIM_BULK_EP" \
     }
   ]
 }')
-
 
 # Check if the response contains any error message
 if echo "$response" | grep -q '"Errors":'; then
