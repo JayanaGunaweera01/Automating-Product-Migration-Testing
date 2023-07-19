2fd9e861c45f09582b4eae5aa69ddd32bf36795f
Jayana  Gunaweera <79576139+JayanaGunaweera01@users.noreply.github.com>
2023-07-19 16:52:35 +0530

    Update download-migration-client.sh

--- migration-automation/download-migration-client.sh
+++ migration-automation/download-migration-client.sh
@@ -1,53 +1,58 @@
 #!/bin/bash
 
-MIGRATION_PAT=$1
+set -euo pipefail
 
-export GITHUB_REPO_OWNER="wso2-extensions"
-export GITHUB_REPO_NAME="identity-migration-resources"
-export TAG="1.0.225"
-export ASSET_FILE_NAME_WITH_EXT="wso2is-migration-$TAG.zip"
+base64url() {
+    echo -n "$1" | base64 | tr -d '\n' | tr '+/' '-_' | tr -d '='
+}
 
-github_tag_url=$(curl --silent --show-error \
-  --header "Authorization: token $MIGRATION_PAT" \
-  --header "Accept: application/json, application/vnd.github.v3+json" \
-  --request GET "https://api.github.com/repos/$GITHUB_REPO_OWNER/$GITHUB_REPO_NAME/releases?per_page=100&draft=false" |
-  jq -r '.url')
-echo "Discovered the github_tag_url: ${github_tag_url}"
+key_json_file="/home/runner/work/getAccessToken/getAccessToken/migration-automation/pure-object-391312-cca370803582.json"
+scope="https://www.googleapis.com/auth/drive.readonly"
+valid_for_sec="${3:-3600}"
+valid_for_sec_numeric=$(echo "$valid_for_sec" | tr -d -c '[:digit:]')
+private_key=$(jq -r .private_key "$key_json_file")
+sa_email=$(jq -r .client_email "$key_json_file") || { echo "Error extracting client_email from the JSON file."; exit 1; }
+echo "Private Key: $private_key"
+echo "Service Account Email: $sa_email"
 
-github_tag_id=$(curl --silent --show-error \
-  --header "Authorization: token $MIGRATION_PAT" \
-  --header "Accept: application/json, application/vnd.github.v3+json" \
-  --request GET "$github_tag_url" | 
-  jq --raw-output "first(.[] | select(.tag_name==\"v$TAG\")).id")
-echo "Discovered the tag_id: ${github_tag_id}"
-echo "Creating the download link..."
+header='{"alg":"RS256","typ":"JWT"}'
+claim=$(cat <<EOF | jq -c
+  {
+    "iss": "$sa_email",
+    "scope": "$scope",
+    "aud": "https://www.googleapis.com/oauth2/v4/token",
+    "exp": $(($(date +%s) + $valid_for_sec_numeric)),
+    "iat": $(date +%s)
+  }
+EOF
+)
 
-# Get the download URL of the desired asset
-download_url=$(curl --silent --show-error \
-  --header "Authorization: token $MIGRATION_PAT" \
-  --header "Accept: application/json, application/vnd.github.v3+json" \
-  --location --request "GET" \
-  "https://api.github.com/repos/$GITHUB_REPO_OWNER/$GITHUB_REPO_NAME/releases/$github_tag_id" |
-  jq --raw-output ".assets[] | select(.name==\"${ASSET_FILE_NAME_WITH_EXT}\").url")
-echo "Found resource download URL: $download_url"
-echo "Discovering the S3 bucket URL for the resource..."
+request_body=$(base64url "$header").$(base64url "$claim")
+signature=$(echo -n "$request_body" | openssl dgst -sha256 -sign <(echo "$private_key") -binary | base64url)
+jwt_token=$(printf "%s.%s\n" "$request_body" "$signature")
 
-redirect_url=$(curl --silent --show-error \
-  --header "Accept: application/octet-stream" \
-  --header "Authorization: token $MIGRATION_PAT" \
-  --request GET --write-out "%{redirect_url}" \
-  "$download_url")
-echo "Download is starting now..."
+echo "JWT Token: $jwt_token"
 
-# Finally, download the actual binary
-curl -LJO --show-error \
-  --output "./utils/migration-client" \
-  --request GET \
-  "$redirect_url"
-echo "Binary $ASSET_FILE_NAME_WITH_EXT download is completed."
+# Make the token request to get the access token
+token_response=$(curl -s -X POST https://www.googleapis.com/oauth2/v4/token \
+    --data-urlencode "grant_type=urn:ietf:params:oauth:grant-type:jwt-bearer" \
+    --data-urlencode "assertion=$jwt_token")
 
-wget -qq "$ASSET_FILE_NAME_WITH_EXT"
-wait $!
-echo "Unzipped downloaded migration client"
-ls -a
+# Check for HTTP 400 errors in the response
+if [[ $token_response == *"HTTP/2 400"* ]]; then
+    echo "HTTP 400 Error: Invalid request or bad syntax."
+    echo "$token_response"
+    exit 1
+fi
 
+# Extract the access token from the response
+access_token=$(echo "$token_response" | jq -r .access_token)
+
+# Check if the access token is null or empty
+if [ -z "$access_token" ]; then
+    echo "Error: Access token is null or empty."
+    echo "$token_response"
+    exit 1
+fi
+
+echo "Access Token: $access_token"
